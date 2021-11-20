ef58ec5f10d59560f73f9c62ca3912ecc8fe5ca8
habvi <a2231250601h@yahoo.co.jp>
2021-11-20 13:23:22 +0900

    add: some file

---
+++ ABC/210/C_ColorfulCandies.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+n, k = map(int, input().split())
+c = list(map(int, input().split()))
+d = defaultdict(int)
+for i in range(k):
+    d[c[i]] += 1
+ans = len(d)
+
+for i in range(k, n):
+    d[c[i-k]] -= 1
+    if d[c[i-k]] == 0:
+        d.pop(c[i-k])
+    d[c[i]] += 1
+    ans = max(ans, len(d))
+print(ans)
