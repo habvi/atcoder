2651629d25fee87ff3c0a684f12587b2bf6581b3
habvi <a2231250601h@yahoo.co.jp>
2021-12-06 09:15:56 +0900

    AGC129_A_ver.2

--- ARC/129/A_SmallerXOR.py
+++ ARC/129/A_SmallerXOR.py
@@ -1,20 +1,10 @@
 n, l, r = map(int, input().split())
-a = []
-for i in range(len(bin(n)[2:])):
-    if (n>>i) & 1 == 1:
-        a.append((1<<i, (1<<(i+1)) - 1))
-
+k = len(bin(n)) - 2
 ans = 0
-for x, y in a:
-    if y < l: continue
-    if r < x: break
-
-    if x < l and r < y:
-        ans += (r - l + 1)
-    elif l <= x and y <= r:
-        ans += (y - x + 1)
-    elif x < l:
-        ans += (y - max(x, l) + 1)
-    else:
-        ans += (min(y, r) - x + 1)
+for i in range(k):
+    if n>>i & 1:
+        kl = 1<<i
+        kr = (1<<i + 1) - 1
+        if kr < l or kl > r: continue
+        ans += min(r, kr) - max(l, kl) + 1
 print(ans)
