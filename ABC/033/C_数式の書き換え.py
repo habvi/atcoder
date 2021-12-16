f6476d6aa0e25b47956b67990d9c2a960f189dc8
habvi <a2231250601h@yahoo.co.jp>
2021-12-16 09:02:05 +0900

    033_B ver.2

--- ABC/033/C_数式の書き換え.py
+++ ABC/033/C_数式の書き換え.py
@@ -1,10 +1,6 @@
 s = input().split('+')
 ans = 0
 for t in s:
-    if len(t) == 1:
-        if t != '0':
-            ans += 1
-    else:
-        if '0' not in t.split('*'):
-            ans += 1
+    if '0' not in t.split('*'):
+        ans += 1
 print(ans)
