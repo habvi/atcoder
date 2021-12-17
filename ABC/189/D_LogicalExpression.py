3c1c670b8de48dcfe0361fcd0d69d3fb08d4af96
habvi <a2231250601h@yahoo.co.jp>
2021-12-18 08:53:34 +0900

    slight update: 189_D

--- ABC/189/D_LogicalExpression.py
+++ ABC/189/D_LogicalExpression.py
@@ -1,18 +1,17 @@
 n = int(input())
-dp = [[0] * 2 for _ in range(n+1)]
+dp = [[0] * 2 for i in range(n + 1)]
 dp[0][0] = 1
 dp[0][1] = 1
-
-S = [input() for _ in range(n)]
 for i in range(n):
-    if S[i] == 'AND':
-        dp[i+1][0&0] += dp[i][0]
-        dp[i+1][0&1] += dp[i][1]
-        dp[i+1][1&0] += dp[i][0]
-        dp[i+1][1&1] += dp[i][1]
+    s = input()
+    if s == 'AND':
+        dp[i + 1][0 & 0] += dp[i][0]
+        dp[i + 1][0 & 1] += dp[i][1]
+        dp[i + 1][1 & 0] += dp[i][0]
+        dp[i + 1][1 & 1] += dp[i][1]
     else:
-        dp[i+1][0|0] += dp[i][0]
-        dp[i+1][0|1] += dp[i][1]
-        dp[i+1][1|0] += dp[i][0]
-        dp[i+1][1|1] += dp[i][1]
-print(dp[-1][1])
+        dp[i + 1][0 | 0] += dp[i][0]
+        dp[i + 1][0 | 1] += dp[i][1]
+        dp[i + 1][1 | 0] += dp[i][0]
+        dp[i + 1][1 | 1] += dp[i][1]
+print(dp[n][1])
