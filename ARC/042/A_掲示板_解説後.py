bfaaff5b036d5a7fd9b2d56da42c4b3efcd0f190
habvi <a2231250601h@yahoo.co.jp>
2021-12-19 10:25:13 +0900

    add: arc042_A

--- ARC/042/A_掲示板_解説後.py
+++ ARC/042/A_掲示板_解説後.py
@@ -1,12 +1,12 @@
 n, m = map(int, input().split())
-s = [0]*n
-ans = []
-A = [int(input()) for _ in range(m)][::-1]
-for i, a in enumerate(A):
-    if s[a-1]: continue
-    ans.append(a)
-    s[a-1] = 1
-    
-ans += [i+1 for i, v in enumerate(s) if v == 0]
-for a in ans:
-    print(a)
+a = [int(input()) for _ in range(m)]
+done = set()
+for b in a[::-1]:
+    if b not in done:
+        print(b)
+        done.add(b)
+
+for i in range(1, n + 1):
+    if i not in done:
+        print(i)
+        done.add(i)
