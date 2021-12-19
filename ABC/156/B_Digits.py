16cf3faa2489f1f218c48ab73ecd248e3649c9ad
habvi <a2231250601h@yahoo.co.jp>
2021-12-20 08:51:36 +0900

    fixed

--- ABC/156/B_Digits.py
+++ ABC/156/B_Digits.py
@@ -1,13 +1,8 @@
-n = int(input())
-a = list(map(int, input().split()))
-ans = 1
-if 0 in a:
-    print(0)
-    exit()
+def x_to_n_base(x, n):
+    if (int(x / n)):
+        return x_to_n_base(int(x / n), n) + str(x % n)
+    return str(x % n)
 
-for i in range(n):
-    ans *= a[i]
-    if ans > 10 ** 18:
-        print(-1)
-        exit()
-print(ans)
+n, k = map(int, input().split())
+a = x_to_n_base(n, k)
+print(len(str(a)))
