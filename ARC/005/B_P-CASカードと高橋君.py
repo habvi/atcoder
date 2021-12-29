c38b0d0b0f02c4f1b4a32f84eb5df9636d8df2ff
habvi <a2231250601h@yahoo.co.jp>
2021-12-29 14:20:49 +0900

    update

--- ARC/005/B_P-CASカードと高橋君.py
+++ ARC/005/B_P-CASカードと高橋君.py
@@ -1,24 +1,17 @@
 x, y, W = input().split()
 x, y = int(x) - 1, int(y) - 1
 c = [input() for _ in range(9)]
-dir = ['R', 'L', 'U', 'D', 'RU', 'RD', 'LU', 'LD']
-dys = [0, 0, -1, 1, -1, 1, -1, 1]
-dxs = [1, -1, 0, 0, 1, 1, -1, -1]
-dy = dys[dir.index(W)]
-dx = dxs[dir.index(W)]
-
-if W in ('U', 'RU', 'LU') and y == 0 or W in ('D', 'RD', 'LD') and y == 8:
-    dy *= -1
-if W in ('L', 'LU', 'LD') and x == 0 or W in ('R', 'RU', 'RD') and x == 8:
-    dx *= -1
+d = {'R':(0, 1), 'L':(0, -1), 'U':(-1, 0), 'D':(1, 0),
+     'RU':(-1, 1), 'RD':(1, 1), 'LU':(-1, -1), 'LD':(1, -1)}
+dy, dx = d[W]
 
 ans = ''
 for i in range(4):
     ans += c[y][x]
+    if (y == 0 and dy < 0) or (y == 8 and dy > 0):
+        dy *= -1
+    if (x == 0 and dx < 0) or (x == 8 and dx > 0):
+        dx *= -1
     y += dy
     x += dx
-    if y in (0, 8):
-        dy *= -1
-    if x in (0, 8):
-        dx *= -1
 print(ans)
