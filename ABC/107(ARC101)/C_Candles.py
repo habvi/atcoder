62c24bc877dd7d2ad43ed3dc297c5f9b844811ee
habvi <a2231250601h@yahoo.co.jp>
2022-01-02 00:43:06 +0900

    update

--- ABC/107(ARC101)/C_Candles.py
+++ ABC/107(ARC101)/C_Candles.py
@@ -1,30 +1,19 @@
-from itertools import accumulate
 n, k = map(int, input().split())
 x = list(map(int, input().split()))
-l, r = [], []
+l, r = [], [0]
 for i in range(n):
     if x[i] <= 0:
-        l.append(x[i])
+        l.append(-x[i])
     else:
         r.append(x[i])
 l.append(0)
-r = [0] + r
-
-dl, dr = [], []
-for i in range(len(l) - 1):
-    dl.append(l[i + 1] - l[i])
-for i in range(len(r) - 1):
-    dr.append(r[i + 1] - r[i])
-
-dl = dl[::-1]
-al = [0] + list(accumulate(dl))
-ar = [0] + list(accumulate(dr))
+l = l[::-1]
 
 ans = float('inf')
-for i in range(len(al)):
+for i in range(len(l)):
     j = k - i
     if j < 0:
         break
-    if j < len(ar):
-        ans = min(ans, al[i]*2 + ar[j], al[i] + ar[j]*2)
+    if j < len(r):
+        ans = min(ans, l[i]*2 + r[j], l[i] + r[j]*2)
 print(ans)
