3cd0bdee9c9112a09a025d21932f907698758582
habvi <a2231250601h@yahoo.co.jp>
2022-01-01 09:55:34 +0900

    update: 178_E

--- ABC/178/E_DistMax.py
+++ ABC/178/E_DistMax.py
@@ -2,9 +2,6 @@ n = int(input())
 a, b = [], []
 for _ in range(n):
     x, y = map(int, input().split())
-    a.append(x + y)
-    b.append(x - y)
-a.sort()
-b.sort()
-ans = max(a[-1] - a[0], b[-1] - b[0])
-print(ans)
+    a.append(x - y)
+    b.append(x + y)
+print(max(max(a) - min(a), max(b) - min(b)))
