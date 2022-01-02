07a4deaea5afcd0a1ad3143863289c79d79373d0
habvi <a2231250601h@yahoo.co.jp>
2022-01-03 00:01:42 +0900

    update

--- ABC/126/D_EvenRelation.py
+++ ABC/126/D_EvenRelation.py
@@ -1,24 +1,25 @@
+from collections import defaultdict
 import sys
-sys.setrecursionlimit(10**7)
-
-def dfs(v, p, cnt):
-    if cnt % 2 == 0:
-        cl[v] = 1
-    for nv, nw in G[v]:
-        if nv == p: continue
-        if nw % 2 == 1:
-            cnt += 1
-        dfs(nv, v, cnt)
-        if nw % 2 == 1:
-            cnt -= 1
+sys.setrecursionlimit(10 ** 7)
+def dfs(v, p):
+    for nv, nw in g[v]:
+        if nv == p:
+            continue
+        if nw % 2 == 0:
+            c[nv] = c[v]
+        else:
+            c[nv] = 1 - c[v]
+        dfs(nv, v)
 
 n = int(input())
-G = [[] for _ in range(n)]
-for _ in range(n-1):
+g = defaultdict(list)
+for _ in range(n - 1):
     u, v, w = map(int, input().split())
-    u, v = u-1, v-1
-    G[u].append((v, w))
-    G[v].append((u, w))
-cl = [0]*n
-dfs(0, -1, 0)
-print(*cl)
+    u, v = u - 1, v - 1
+    g[u].append((v, w))
+    g[v].append((u, w))
+
+c = [-1] * n
+c[0] = 0
+dfs(0, -1)
+print(*c)
