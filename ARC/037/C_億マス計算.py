516fd1fe894778aed8ba046b33d8eab70b484a8d
habvi <a2231250601h@yahoo.co.jp>
2022-01-02 18:15:27 +0900

    add: 037_C

---
+++ ARC/037/C_億マス計算.py
@@ -0,0 +1,24 @@
+from bisect import bisect
+n, K = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.sort()
+b.sort()
+
+def is_ok(x):
+    tot = 0
+    for i in range(n):
+        tot += bisect(b, x // a[i])
+    return tot < K
+
+def bisect2(ng, ok):
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+bi = bisect2(a[-1] * b[-1] + 1, 0)
+print(bi + 1)
