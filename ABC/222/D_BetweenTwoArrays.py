aca4356c3cb89e674733a52b1ccad4907a2cd819
habvi <a2231250601h@yahoo.co.jp>
2022-01-04 23:44:27 +0900

    update

--- ABC/222/D_BetweenTwoArrays.py
+++ ABC/222/D_BetweenTwoArrays.py
@@ -1,17 +1,22 @@
-from itertools import accumulate
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 MOD = 998244353
 
+def accumulate_mod(lis, MOD):
+    ac = [lis[0] % MOD]
+    for x in lis[1:]:
+        ac.append((ac[-1] + x) % MOD)
+    return ac
+
 dp = [0] * 3001
 for i in range(a[0], b[0] + 1):
     dp[i] += 1
-dp = list(accumulate(dp))
+dp = accumulate_mod(dp, MOD)
 
 for i in range(1, n):
     ndp = [0] * 3001
     for j in range(a[i], b[i] + 1):
-        ndp[j] = dp[j] % MOD
-    dp = list(accumulate(ndp))
-print(dp[b[-1]] % MOD)
+        ndp[j] = dp[j]
+    dp = accumulate_mod(ndp, MOD)
+print(dp[b[-1]])
