4e2030792f4ea7ce8725eb707fdf711992e54611
habvi <a2231250601h@yahoo.co.jp>
2022-01-05 19:40:19 +0900

    add: hhkb2020_B

---
+++ ABC-Like/hhkb2020/B_Futon.py
@@ -0,0 +1,12 @@
+h, w = map(int, input().split())
+s = [input() for _ in range(h)]
+ans = 0
+for i in range(h):
+    for j in range(w):
+        if s[i][j] == "#":
+            continue
+        if j + 1 < w and s[i][j + 1] == ".":
+            ans += 1
+        if i + 1 < h and s[i + 1][j] == ".":
+            ans += 1
+print(ans)
