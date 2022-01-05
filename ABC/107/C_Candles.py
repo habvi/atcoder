a67f4af4f49897f84c63c9490612245119d6f848
habvi <a2231250601h@yahoo.co.jp>
2022-01-05 21:12:08 +0900

    update

--- ABC/107/C_Candles.py
+++ ABC/107/C_Candles.py
@@ -1,21 +1,17 @@
 n, k = map(int, input().split())
 X = list(map(int, input().split()))
-l, r = [], []
+l, r = [], [0]
 for x in X:
     if x < 0:
         l.append(-x)
     else:
         r.append(x)
+l.append(0)
+l = l[::-1]
 
 ans = float('inf')
-for i in range(n - k + 1):
-    j = i + k - 1
-    if j < len(l):
-        ans = min(ans, l[i])
-    elif i >= len(l):
-        j -= len(l)
-        ans = min(ans, r[j])
-    else:
-        j -= len(l)
-        ans = min(ans, l[i] * 2 + r[j], l[i] + r[j] * 2)
+for i in range(len(l)):
+    j = k - i
+    if 0 <= j < len(r):
+        ans = min(ans, l[i]*2 + r[j], l[i] + r[j]*2)
 print(ans)
