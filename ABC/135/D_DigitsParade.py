0dbb8d83b52666ceb91eb3a525558f7d48b7788e
habvi <a2231250601h@yahoo.co.jp>
2022-01-11 02:25:14 +0900

    update

--- ABC/135/D_DigitsParade.py
+++ ABC/135/D_DigitsParade.py
@@ -1,20 +1,23 @@
-s = input()
-n = len(s)
+S = input()
 MOD = 10**9 + 7
 dp = [0] * 13
 dp[0] = 1
-for i in range(n - 1, -1, -1):
-    p = pow(10, n - i - 1, 13)
+b = 1
+for s in S[::-1]:
     ndp = [0] * 13
-
-    if s[i] == '?':
+    if s == '?':
         for k in range(10):
-            for j in range(13):
-                ndp[(k*p + j) % 13] += dp[j]
-                ndp[(k*p + j) % 13] %= MOD
+            for i in range(13):
+                idx = (k*b + i) % 13
+                ndp[idx] += dp[i]
+                ndp[idx] %= MOD
     else:
-        for j in range(13):
-            ndp[(int(s[i])*p + j) % 13] += dp[j]
-            ndp[(int(s[i])*p + j) % 13] %= MOD
+        s = int(s)
+        for i in range(13):
+            idx = (s*b + i) % 13
+            ndp[idx] += dp[i]
+            ndp[idx] %= MOD
+    b *= 10
+    b %= 13
     dp = ndp
 print(dp[5])
