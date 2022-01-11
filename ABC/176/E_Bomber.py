9aa3738f10279f6fe576ef8567cbe0aa5af71675
habvi <a2231250601h@yahoo.co.jp>
2022-01-12 00:28:27 +0900

    refactor

--- ABC/176/E_Bomber.py
+++ ABC/176/E_Bomber.py
@@ -1,29 +1,23 @@
 h, w, m = map(int, input().split())
-g = set()
+bomb = set()
 row = [0] * h
 col = [0] * w
 for _ in range(m):
-    y, x = map(int, input().split())
-    y -= 1; x -= 1
+    y, x = map(lambda x: int(x) - 1, input().split())
+    bomb.add((y, x))
     row[y] += 1
     col[x] += 1
-    g.add((y, x))
 
-mr, mc = max(row), max(col)
-ri, ci = [], []
-for i, r in enumerate(row):
-    if r == mr:
-        ri.append(i)
-for i, c in enumerate(col):
-    if c == mc:
-        ci.append(i)
+mxr = max(row)
+ri = [i for i, r in enumerate(row) if r == mxr]
 
-ans = mr + mc
-for r in ri:
-    for c in ci:
-        if (r, c) in g:
-            continue
-        print(ans)
-        exit()
-ans -= 1
-print(ans)
+mxc = max(col)
+ci = [i for i, c in enumerate(col) if c == mxc]
+
+ans = mxr + mxc
+for i in ri:
+    for j in ci:
+        if (i, j) not in bomb:
+            print(ans)
+            exit()
+print(ans - 1)
