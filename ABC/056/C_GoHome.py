936d0ce833f7281c277c7417a6133b7124139fb5
habvi <a2231250601h@yahoo.co.jp>
2022-01-13 23:42:08 +0900

    another solution

--- ABC/056/C_GoHome.py
+++ ABC/056/C_GoHome.py
@@ -1,6 +1,16 @@
+# O(1)
 x = int(input())
 a = int((x * 2) ** 0.5)
 if a * (a + 1) >= x * 2:
     print(a)
 else:
     print(a + 1)
+
+
+
+# O(√N)
+# x = int(input())
+# for i in range(1, int((x * 2)**0.5) + 5):
+#     if i * (i + 1) >= 2 * x:
+#         print(i)
+#         exit()
