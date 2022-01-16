2261f932cd0c296c368354662e19f728dd9237f6
habvi <a2231250601h@yahoo.co.jp>
2022-01-16 20:03:16 +0900

    refactor: for4 -> for3

--- ABC/234/E_ArithmeticNumber.py
+++ ABC/234/E_ArithmeticNumber.py
@@ -1,25 +1,20 @@
-X = input()
-n = len(X)
-a = set()
-en = 0
-for d in range(-8, 9):
-    for f in range(1, 10):
-        for i in range(1, 18):
-            t = [str(f)]
-            for _ in range(i):
-                nxt = int(t[-1]) + d
-                if 0 <= nxt <= 9:
-                    t.append(str(nxt))
-                else:
-                    en = 1
-                    break
-            a.add(int("".join(t)))
-            if en:
-                en = 0
-                break
-a = list(a)
-a.sort()
+x = input()
+n = len(x)
+lis = []
+fin = False
+for fst in range(1, 10):
+    lis.append(fst)
+    for diff in range(-9, 9):
+        t = fst
+        while not fin and len(str(t)) <= n:
+            if 0 <= t % 10 + diff <= 9:
+                t = t * 10 + t % 10 + diff
+                lis.append(t)
+            else:
+                fin = True
+        fin = False
+lis.sort()
 
 from bisect import bisect_left
-bi = bisect_left(a, int(X))
-print(a[bi])
+bi = bisect_left(lis, int(x))
+print(lis[bi])
