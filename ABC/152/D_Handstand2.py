7d969af83bd8452b936eea6466b26eb87f0dc852
habvi <a2231250601h@yahoo.co.jp>
2022-01-17 23:55:43 +0900

    refactor

--- ABC/152/D_Handstand2.py
+++ ABC/152/D_Handstand2.py
@@ -1,17 +1,13 @@
 from collections import defaultdict
 n = int(input())
-k = len(str(n))
-d = defaultdict(list)
+d = defaultdict(int)
 for i in range(1, n + 1):
-    f = str(i)[0]
-    b = str(i)[-1]
-    if f == 0 or b == 0:
-        continue
-    d[(int(f), int(b))].append(i)
+    fr = str(i)[0]
+    tl = str(i)[-1]
+    d[(fr, tl)] += 1
 
 ans = 0
-for i in range(1, 10):
-    for j in range(1, 10):
-        if (i, j) in d and (j, i) in d:
-            ans += len(d[(i, j)]) * len(d[(j, i)])
+for (ft, tl), c in d.items():
+    if (tl, ft) in d:
+        ans += c * d[(tl, ft)]
 print(ans)
