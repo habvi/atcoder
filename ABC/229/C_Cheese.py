a5a9d8a38f34334472d1a54c050a823fd96cd958
habvi <a2231250601h@yahoo.co.jp>
2022-01-17 11:41:22 +0900

    refactor

--- ABC/229/C_Cheese.py
+++ ABC/229/C_Cheese.py
@@ -1,19 +1,14 @@
 n, w = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
-a.sort(reverse=True)
-g = 0
+ab = [tuple(map(int, input().split())) for _ in range(n)]
+ab.sort(key=lambda x: x[0], reverse=True)
+
+tg = 0
 ans = 0
-for i in range(n):
-    if g > w:
-        break
-    if g + a[i][1] > w: 
-        for j in range(a[i][1]):
-            if g + 1 <= w:
-                ans += a[i][0]
-                g += 1
-            else:
-                break
+for y, g in ab:
+    if tg + g <= w:
+        tg += g
+        ans += y * g
     else:
-        ans += a[i][0] * a[i][1]
-        g += a[i][1]
+        ans += y * (w - tg)
+        break
 print(ans)
