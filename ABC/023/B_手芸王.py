4a3847db2ac38ee5439407caea0261f09975a05c
habvi <a2231250601h@yahoo.co.jp>
2022-01-17 10:49:31 +0900

    refactor

--- ABC/023/B_手芸王.py
+++ ABC/023/B_手芸王.py
@@ -1,13 +1,13 @@
 n = int(input())
 s = input()
 t = 'b'
-a = [t]
-for i in range(102):
+ans = 0
+for i in range(n // 2):
     if i % 3 == 0:
         t = "".join(('a', t, 'c'))
     elif i % 3 == 1:
         t = "".join(('c', t, 'a'))
     else:
         t = "".join(('b', t, 'b'))
-    a.append(t)
-print(a.index(s) if s in a else -1)
+    ans += 1
+print(ans if s == t else -1)
