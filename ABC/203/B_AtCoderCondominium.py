02017ea978eb350e3c4a5d1b681cf9dffe9b5803
habvi <a2231250601h@yahoo.co.jp>
2022-01-18 23:52:54 +0900

    add: another O(1) solution

--- ABC/203/B_AtCoderCondominium.py
+++ ABC/203/B_AtCoderCondominium.py
@@ -4,3 +4,11 @@ for i in range(1, n + 1):
     for j in range(1, k + 1):
         ans += i * 100 + j
 print(ans)
+
+
+
+# O(1)
+# def f(x):
+#     return x * (x + 1) // 2
+# n, k = map(int, input().split())
+# print(100 * f(n) * k + f(k) * n)
