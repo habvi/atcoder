1f9b83ed421981e5ad2ff36b6d192713f85e0268
habvi <a2231250601h@yahoo.co.jp>
2022-01-20 17:37:02 +0900

    add: 160_D

---
+++ ABC/160/D_Line++.py
@@ -0,0 +1,8 @@
+n, x, y = map(int, input().split())
+x, y = x - 1, y - 1
+
+d = [0] * (n - 1)
+for i in range(n):
+    for j in range(i + 1, n):
+        d[min(j - i, abs(i - x) + 1 + abs(j - y)) - 1] += 1
+print(*d)
