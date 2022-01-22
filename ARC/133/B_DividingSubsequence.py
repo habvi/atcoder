c2b356ed99b7e974b08ef6052f9cf7b45dbc220a
habvi <a2231250601h@gmail.com>
2022-01-23 03:04:00 +0900

    refactor

--- ARC/133/B_DividingSubsequence.py
+++ ARC/133/B_DividingSubsequence.py
@@ -1,13 +1,13 @@
+from bisect import bisect_left
 def div_lis(x):
-    div_l, div_r = [], []
+    div = []
     i = 1
     while i * i <= x:
         if x % i == 0:
-            div_l.append(ai[i])
+            div.append(ai[i])
             if i != x // i:
-                div_r.append(ai[x // i])
+                div.append(ai[x // i])
         i += 1
-    div = div_l + div_r[::-1]
     return div
 
 n = int(input())
@@ -15,7 +15,6 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 ai = {a : i for i, a in enumerate(A)}
 
-from bisect import bisect, bisect_left
 lis = [float('inf')] * (n + 1)
 for b in B:
     div = div_lis(b)
