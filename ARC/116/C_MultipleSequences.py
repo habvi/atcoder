cc404566664a0a721b7b4583a088deb9fc9ee8e8
habvi <a2231250601h@gmail.com>
2022-01-27 00:05:42 +0900

    add: arc116_C

---
+++ ARC/116/C_MultipleSequences.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+def Sieve(x):
+    memo = [0] * (x + 1)
+    for i in range(2, x + 1):
+        if memo[i]: continue
+        memo[i] = i
+        for j in range(i * i, x + 1, i):
+            if memo[j]: continue
+            memo[j] = i
+    return memo
+
+def comb(n, r, MOD):
+    if n < r or n < 0 or r < 0:
+        return 0
+    nmrt = fact[n]
+    dnmnt = invfact[n - r] * invfact[r] % MOD
+    return nmrt * dnmnt % MOD
+
+n, m = map(int, input().split())
+MOD = 998244353
+mx = 3 * 10**5
+
+memo = Sieve(mx)
+
+lenc = mx
+fact, invfact = [1] * lenc, [1] * lenc
+for i in range(1, lenc):
+    fact[i] = fact[i - 1] * i % MOD
+
+invfact[lenc - 1] = pow(fact[lenc - 1], MOD - 2, MOD)
+for i in range(lenc - 1, 0, -1):
+    invfact[i-1] = invfact[i] * i % MOD
+
+ans = 0
+for i in range(1, m + 1):
+    d = defaultdict(int)
+    while i > 1:
+        d[memo[i]] += 1
+        i //= memo[i]
+    
+    tot = 1
+    for k, v in d.items():
+        tot *= comb(n + v - 1, v, MOD)
+        tot %= MOD
+    ans += tot
+    ans %= MOD
+print(ans)
