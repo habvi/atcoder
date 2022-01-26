be9eda6f0b408e6282a600b3baa47ed8ea1d3b2c
habvi <a2231250601h@gmail.com>
2022-01-26 22:57:03 +0900

    add: another way

--- ABC/185/C_DuodecimFerra.py
+++ ABC/185/C_DuodecimFerra.py
@@ -7,3 +7,15 @@ for i in range(1, 12):
     L -= 1
     dnmnt *= i
 print(nmrt // dnmnt)
+
+
+
+# from math import factorial
+# def P(n, r):
+#     return factorial(n) // factorial(n - r)
+
+# def C(n,r):
+#     return P(n, r) // factorial(r)
+
+# L = int(input())
+# print(C(L - 1, 11))
