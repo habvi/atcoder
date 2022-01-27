fe2c0069056be0434edaddad0e7e7aa84e96b97e
habvi <a2231250601h@yahoo.co.jp>
2022-01-27 22:41:35 +0900

    refactor

--- ABC/187/C_1-SAT.py
+++ ABC/187/C_1-SAT.py
@@ -1,16 +1,8 @@
 n = int(input())
-
-a = set()
-b = set()
-for _ in range(n):
-    s = input()
-    if s[0] == '!':
-        a.add(s[1:])
-    else:
-        b.add(s)
+S = set([input() for _ in range(n)])
         
-for t in b:
-    if t in a:
-        print(t)
+for s in S:
+    if ''.join(('!', s)) in S:
+        print(s)
         exit()
 print('satisfiable')
