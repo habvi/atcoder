0d16cd6041f4e8b0d940c9712437a3f553c82c02
habvi <a2231250601h@yahoo.co.jp>
2022-01-27 23:01:58 +0900

    refactor

--- OtherSponsored/sumitrust2019/D_LuckyPIN.py
+++ OtherSponsored/sumitrust2019/D_LuckyPIN.py
@@ -1,17 +1,15 @@
 n = int(input())
 S = input()
-k1 = [0]*10
-k2 = set()
-k3 = set()
+
+one = set()
+two = set()
+three = set()
 for s in S:
-    add_k2 = set()
-    for i, k in enumerate(k1):
-        if k:
-            add_k2.add(str(i) + s)
-
-    k1[int(s)] = 1
-    for k in k2:
-        k3.add(k + s)
-    k2 |= add_k2
-
-print(len(k3))
+    for t in two:
+        three.add(t + s)
+        
+    for t in one:
+        two.add(t + s)
+    
+    one.add(s)
+print(len(three))
