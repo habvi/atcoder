8a74ce8b6ba1af0c8455c5b9921744ce4aa4031c
habvi <a2231250601h@yahoo.co.jp>
2022-01-28 17:28:24 +0900

    add: arc040_B

---
+++ ARC/040/B_直線塗り.py
@@ -0,0 +1,25 @@
+n, r = map(int, input().split())
+S = input()
+
+if S.count('o') == n:
+    print(0)
+    exit()
+
+ls = list(S)
+tail = S.rfind('.')
+
+ans = 0
+for i, s in enumerate(ls):
+    if i + r - 1 >= tail:
+        ans += 1 if i == 0 else 2
+        break
+    
+    if s == '.':
+        ans += 1
+        for j in range(r):
+            if i + j < len(ls):
+                ls[i + j] = 'o'
+    if i != 0:
+        ans += 1
+    
+print(ans)
