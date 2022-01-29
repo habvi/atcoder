27f119dd5f8365b79226cb23baa5acab3cf6c096
habvi <a2231250601h@yahoo.co.jp>
2022-01-30 01:42:56 +0900

    refactor

--- ARC/134/B_Reserve-or-Reverse.py
+++ ARC/134/B_Reserve-or-Reverse.py
@@ -2,38 +2,39 @@ from collections import defaultdict
 
 n = int(input())
 S = input()
-T = list(S)
 
 d = defaultdict(list)
 for i, s in enumerate(S):
     d[s].append(i)
 
+ans = list(S)
 mx = n - 1
 for i, s in enumerate(S):
     if mx <= i:
         break
     
     for al in range(ord('a'), ord('z') + 1):
-        if al >= ord(s):
+        minc = chr(al)
+        if minc >= s:
             break
-        if chr(al) not in d:
+        if minc not in d:
             continue
 
-        while d[chr(al)] and d[chr(al)][-1] > mx:
-            d[chr(al)].pop()
-        if not d[chr(al)]:
-            del d[chr(al)]
+        while d[minc] and d[minc][-1] > mx:
+            d[minc].pop()
+        if not d[minc]:
+            del d[minc]
             continue
         
-        idx = d[chr(al)][-1]
-        d[chr(al)].pop()
-        if not d[chr(al)]:
-            del d[chr(al)]
+        swap = d[minc][-1]
+        d[minc].pop()
+        if not d[minc]:
+            del d[minc]
             
-        if idx <= i:
+        if swap <= i:
             continue
         
-        T[idx], T[i] = T[i], T[idx]
-        mx = idx
+        ans[i], ans[swap] = ans[swap], ans[i]
+        mx = swap
         break
-print(''.join(T))
+print(''.join(ans))
