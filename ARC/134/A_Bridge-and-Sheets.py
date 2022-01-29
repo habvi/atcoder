ac0c09670f834b39077ed217ca8b1c54da71e9c2
habvi <a2231250601h@yahoo.co.jp>
2022-01-30 01:53:04 +0900

    add: arc134_A

---
+++ ARC/134/A_Bridge-and-Sheets.py
@@ -0,0 +1,15 @@
+n, l, w = map(int, input().split())
+A = list(map(int, input().split()))
+
+now = 0
+ans = 0
+for a in A:
+    if a > now:
+        must = (a - now + w - 1) // w
+        ans += must
+        now = a + w
+    else:
+        now = a + w
+
+ans += (l - now + w - 1) // w
+print(ans)
