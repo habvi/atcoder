f2d7cf670186e7a6d320ac26e9fecc55f0cb785b
habvi <a2231250601h@yahoo.co.jp>
2022-01-30 15:07:40 +0900

    add: stack ver

---
+++ ARC/134/B_Reserve-or-Reverse_stack.py
@@ -0,0 +1,17 @@
+n = int(input())
+S = input()
+
+nxt = [(i, s) for i, s in enumerate(S)]
+nxt.sort(key=lambda x: (x[1], -x[0]), reverse=True)
+
+ans = list(S)
+r = n
+for l, s in enumerate(S):
+    while nxt and nxt[-1][1] < s:
+        i, _ = nxt.pop()
+        if l < i < r:
+            ans[l], ans[i] = ans[i], ans[l]
+            r = i
+            break
+    
+print(''.join(ans))
