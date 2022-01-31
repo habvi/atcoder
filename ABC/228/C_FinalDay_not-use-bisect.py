e8cb65448e8b41799e994ac2f70086d1830771be
habvi <a2231250601h@yahoo.co.jp>
2022-01-31 22:30:14 +0900

    add: not use bisect ver

---
+++ ABC/228/C_FinalDay_not-use-bisect.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+P = [tuple(map(int, input().split())) for _ in range(n)]
+
+score = [sum(p) for p in P]
+cmp = sorted(score)[-k]
+
+for s in score:    
+    print('Yes' if s + 300 >= cmp else 'No')
