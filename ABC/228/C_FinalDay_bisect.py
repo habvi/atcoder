c721cad94e16b1589a8bff2b455ae84d433f192a
habvi <a2231250601h@yahoo.co.jp>
2022-01-31 22:26:35 +0900

    refactor

---
+++ ABC/228/C_FinalDay_bisect.py
@@ -0,0 +1,13 @@
+from bisect import bisect
+
+n, k = map(int, input().split())
+P = [tuple(map(int, input().split())) for _ in range(n)]
+
+score = [sum(p) for p in P]
+ss = sorted(score)
+
+for s in score:    
+    if n - bisect(ss, s + 300) + 1 <= k:
+        print('Yes')
+    else:
+        print('No')
