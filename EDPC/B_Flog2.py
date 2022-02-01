4f8718193c9c17264d172e0c0cce8eb2779c4103
habvi <a2231250601h@yahoo.co.jp>
2022-02-01 14:11:39 +0900

    add: edpc-B

---
+++ EDPC/B_Flog2.py
@@ -0,0 +1,12 @@
+n, K = map(int, input().split())
+H = list(map(int, input().split()))
+
+dp = [float('inf')] * n
+dp[0] = 0
+
+for i in range(n):
+    for k in range(1, K + 1):
+        if i + k < n:
+            dp[i + k] = min(dp[i + k], dp[i] + abs(H[i] - H[i + k]))
+            
+print(dp[-1])
