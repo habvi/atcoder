caeb30659677bc0c7a37281e80df12e465140432
habvi <a2231250601h@yahoo.co.jp>
2022-02-01 13:27:43 +0900

    slight fix

--- EDPC/A_Frog1.py
+++ EDPC/A_Frog1.py
@@ -1,10 +1,13 @@
 n = int(input())
-h = list(map(int, input().split()))
+H = list(map(int, input().split()))
+
 dp = [float('inf')] * n
 dp[0] = 0
+
 for i in range(n):
     if i + 1 < n:
-        dp[i + 1] = min(dp[i + 1], dp[i] + abs(h[i + 1] - h[i]))
+        dp[i + 1] = min(dp[i + 1], dp[i] + abs(H[i + 1] - H[i]))
     if i + 2 < n:
-        dp[i + 2] = min(dp[i + 2], dp[i] + abs(h[i + 2] - h[i]))
+        dp[i + 2] = min(dp[i + 2], dp[i] + abs(H[i + 2] - H[i]))
+
 print(dp[n - 1])
