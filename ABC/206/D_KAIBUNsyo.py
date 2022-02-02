99861eaf16dc8ddb4429c84b5f4f6785243087a9
habvi <a2231250601h@yahoo.co.jp>
2022-02-02 22:03:43 +0900

    refactor

--- ABC/206/D_KAIBUNsyo.py
+++ ABC/206/D_KAIBUNsyo.py
@@ -1,28 +1,35 @@
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10 ** 7)
 
 def root(x):
-    if rank[x] < 0: return x
+    if rank[x] < 0:
+       return x
     rank[x] = root(rank[x])
     return rank[x]
 def unite(x, y):
     x, y = root(x), root(y)
-    if x == y: return False
-    if rank[x] > rank[y]: x, y = y, x
+    if x == y:
+        return False
+    if rank[x] > rank[y]:
+        x, y = y, x
     rank[x] += rank[y]
     rank[y] = x
     return True
-def is_same(x, y): return root(x) == root(y)
-def size(x): return -rank[root(x)]
+def is_same(x, y):
+    return root(x) == root(y)
+def size(x):
+    return -rank[root(x)]
+
 
 n = int(input())
-a = list(map(int, input().split()))
-rank = [-1] * (2*10**5+1)
+A = list(map(int, input().split()))
+
+rank = [-1] * (max(A) + 1)
 ans = 0
 for i in range(n // 2):
-    u, v = a[i], a[n - i - 1]
-    if is_same(u, v):
-        continue
-    unite(u, v)
-    ans += 1
+    l, r = A[i], A[-i - 1]
+    if not is_same(l, r):
+        unite(l, r)
+        ans += 1
+ 
 print(ans)
