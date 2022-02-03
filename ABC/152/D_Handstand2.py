8a0e7097f92d8fe2a16b4ebda68e7d97023fd764
habvi <a2231250601h@yahoo.co.jp>
2022-02-03 21:27:58 +0900

    add: another solution

--- ABC/152/D_Handstand2.py
+++ ABC/152/D_Handstand2.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
+
 n = int(input())
 d = defaultdict(int)
+
 for i in range(1, n + 1):
     fr = str(i)[0]
     tl = str(i)[-1]
@@ -11,3 +13,12 @@ for (ft, tl), c in d.items():
     if (tl, ft) in d:
         ans += c * d[(tl, ft)]
 print(ans)
+
+
+
+# ans = 0
+# for i in range(1, n + 1):
+#     if i % 10 == 0:
+#         continue
+#     ans += d[(str(i)[-1], str(i)[0])]
+# print(ans)
