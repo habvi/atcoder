ffe9cb1f78e6524bd82f9d3f8e98e80224f83591
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 02:03:13 +0900

    add: 238_B

---
+++ ABC/238/B_Pizza.py
@@ -0,0 +1,18 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+S = []
+for a in A:
+    S.append(0)
+    T = []
+    for s in S:
+        T.append((s + a) % 360)
+    S = T
+
+S.sort()
+S = [0, *S, 360]
+
+ans = 0
+for i in range(len(S) - 1):
+    ans = max(ans, S[i + 1] - S[i])
+print(ans)
