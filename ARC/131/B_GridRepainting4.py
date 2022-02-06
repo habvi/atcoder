af8dd7261b258a7305639ae9cc7f8b52bf7ad3cb
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 11:20:38 +0900

    refactor

--- ARC/131/B_GridRepainting4.py
+++ ARC/131/B_GridRepainting4.py
@@ -4,18 +4,18 @@ g = [list(input()) for _ in range(h)]
 for i in range(h):
     for j in range(w):
         if g[i][j] != '.': continue
-        s = set()
+
+        used = set()
         for dy, dx in zip((0, 1, 0, -1), (1, 0, -1, 0)):
             ny, nx = i + dy, j + dx
-            if not (0 <= ny < h and 0 <= nx < w): continue
-            if g[ny][nx] == '.': continue
-            s.add(g[ny][nx])
-
-        for c in ('1', '2', '3', '4', '5'):
-            if c in s:
+            if not (0 <= ny < h and 0 <= nx < w) or g[ny][nx] == '.':
                 continue
-            g[i][j] = c
-            break
+            used.add(g[ny][nx])
 
-for gg in g:
-    print(''.join(gg))
+        for k in range(1, 6):
+            if str(k) not in used:
+                g[i][j] = str(k)
+                break
+
+for ans in g:
+    print(''.join(ans))
