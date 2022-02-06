60c0cc7e1fb5863560561c16c559e42fafa8afaf
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 22:52:19 +0900

    add: another way

--- ABC/133/C_RemainderMinimization2019.py
+++ ABC/133/C_RemainderMinimization2019.py
@@ -1,9 +1,25 @@
 l, r = map(int, input().split())
 MOD = 2019
-a = [0]*MOD
 
-for i in range(l, min(r, l + MOD)):
-    for j in range(i+1, min(r+1, l + MOD)):
-        a[i*j % MOD] = 1
+ans = 2020
+mx = l + MOD
+for i in range(l, min(r + 1, mx + 1)):
+    for j in range(i + 1, min(r + 1, mx + 1)):
+        ans = min(ans, i * j % MOD)
 
-print(a.index(1))
+print(ans)
+
+
+
+# l, r = map(int, input().split())
+# MOD = 2019
+
+# ans = 2020
+# for i in range(l, r + 1):
+#     for j in range(i + 1, r + 1):
+#         if ans == 0:
+#             print(ans)
+#             exit()
+
+#         ans = min(ans, i * j % MOD)
+# print(ans)
