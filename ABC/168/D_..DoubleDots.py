50069d39da98c9b49d1e86f9564ba6e45737fd92
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 18:05:08 +0900

    refactor

--- ABC/168/D_..DoubleDots.py
+++ ABC/168/D_..DoubleDots.py
@@ -1,16 +1,18 @@
 from collections import deque, defaultdict
+
 def bfs(u):
     dist[u] = 0
-    que = deque([])
-    que.append(u)
-    while que:
-        v = que.popleft()
+    q = deque([])
+    q.append(u)
+    while q:
+        v = q.popleft()
         for nv in g[v]:
-            if dist[nv] != -1: continue
+            if dist[nv] != -1:
+                continue
             dist[nv] = dist[v] + 1
-            par[nv] = v
-            que.append(nv)
-    return dist
+            ans[nv] = v
+            q.append(nv)
+
 
 n, m = map(int, input().split())
 g = defaultdict(list)
@@ -18,14 +20,10 @@ for _ in range(m):
     a, b = map(lambda x: int(x) - 1, input().split())
     g[a].append(b)
     g[b].append(a)
+
 dist = [-1] * n
-par = [-1] * n
+ans = [-1] * n
 bfs(0)
 
-par = par[1:]
-if -1 in par:
-    print('No')
-else:
-    print('Yes')
-    for ans in par:
-        print(ans + 1)
+print('Yes')
+print(*ans[1:])
