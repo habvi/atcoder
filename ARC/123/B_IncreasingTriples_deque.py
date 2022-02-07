0faf19b7da54cbf17f5ba02653cff2ce823dc08a
habvi <a2231250601h@yahoo.co.jp>
2022-02-07 10:28:30 +0900

    refactor

--- ARC/123/B_IncreasingTriples_deque.py
+++ ARC/123/B_IncreasingTriples_deque.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 n = int(input())
 A = sorted(map(int, input().split()))
 B = deque(sorted(map(int, input().split())))
@@ -8,17 +9,16 @@ ans = 0
 for a in A:
     while B and B[0] <= a:
         B.popleft()
+
     if not B:
-        print(ans) 
-        exit()
-        
+        break
     b = B.popleft()
+
     while C and C[0] <= b:
         C.popleft()
     if not C:
-        print(ans)
-        exit()
-    
-    ans += 1
+        break
     C.popleft()
+
+    ans += 1
 print(ans)
