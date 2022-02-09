363d9ad0703ad6e6a76e94179d054a7bd9c9ebb9
habvi <a2231250601h@yahoo.co.jp>
2022-02-10 01:44:52 +0900

    add: 200_D

--- ABC/200/D_HappyBirthday!2.py
+++ ABC/200/D_HappyBirthday!2.py
@@ -2,24 +2,35 @@ n = int(input())
 A = list(map(int, input().split()))
 MOD = 200
 
-m = min(n, 8)
-for i in range(1, 1<<m):
-    for j in range(1, 1<<m):
-        if i == j: continue
-        b, c = [], []
-        sum_b, sum_c = 0, 0
-        for k in range(m):
-            if (i>>k) & 1:
-                b.append(k + 1)
-                sum_b += A[k]
-                sum_b %= MOD
-            if (j>>k) & 1:
-                c.append(k + 1)
-                sum_c += A[k]
-                sum_c %= MOD
-        if sum_b == sum_c:
+# 2**8 - 1 = 255 > 200
+n = min(n, 8)
+A = [a % MOD for a in A[:n]]
+
+for b in range(1, 1 << n):
+    for c in range(1, 1 << n):
+        if b == c:
+            continue
+
+        B = []
+        sumb = 0
+        for i in range(n):
+            if b >> i & 1:
+                B.append(i + 1)
+                sumb += A[i]
+                sumb %= MOD
+
+        C = []
+        sumc = 0
+        for i in range(n):
+            if c >> i & 1:
+                C.append(i + 1)
+                sumc += A[i]
+                sumc %= MOD
+
+        if sumb == sumc:
             print('Yes')
-            print(len(b), *b)
-            print(len(c), *c)
+            print(len(B), *B)
+            print(len(C), *C)
             exit()
+
 print('No')
