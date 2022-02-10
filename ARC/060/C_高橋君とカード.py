ed4cf0bcc98ba91b0ab79d0d77d1a28da41f7f53
habvi <a2231250601h@yahoo.co.jp>
2022-02-10 22:10:11 +0900

    add: arc060_C

---
+++ ARC/060/C_高橋君とカード.py
@@ -0,0 +1,19 @@
+n, A = map(int, input().split())
+X = list(map(int, input().split()))
+
+mx = 2505
+dp = [[[0] * (mx + 1) for _ in range(n + 1)] for _ in range(n + 1)]
+dp[0][0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        for k in range(mx + 1):
+            dp[i + 1][j][k] += dp[i][j][k]
+            if k + X[i] <= mx:
+                dp[i + 1][j + 1][k + X[i]] += dp[i][j][k]
+
+ans = 0
+for i in range(1, n + 1):
+    ans += dp[n][i][i * A]
+
+print(ans)
