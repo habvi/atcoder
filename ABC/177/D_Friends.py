2b3c476dddd5f4f5db05952dba127de42a38ffcf
habvi <a2231250601h@yahoo.co.jp>
2022-02-11 00:04:14 +0900

    add: 177_D

---
+++ ABC/177/D_Friends.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+
+def root(x):
+    if rank[x] < 0:
+       return x
+    rank[x] = root(rank[x])
+    return rank[x]
+def unite(x, y):
+    x, y = root(x), root(y)
+    if x == y:
+        return False
+    if rank[x] > rank[y]:
+        x, y = y, x
+    rank[x] += rank[y]
+    rank[y] = x
+    return True
+def is_same(x, y):
+    return root(x) == root(y)
+def size(x):
+    return -rank[root(x)]
+
+
+n, m = map(int, input().split())
+rank = [-1] * n
+for _ in range(m):
+    a, b = map(lambda x: int(x) - 1, input().split())
+    unite(a, b)
+
+ans = 0
+for i in range(n):
+    if i == root(i):
+        ans = max(ans, size(root(i)))
+print(ans)
