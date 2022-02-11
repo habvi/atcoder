b2b965069bf5592f17cc48648f0d794b4d7fdf6a
habvi <a2231250601h@yahoo.co.jp>
2022-02-11 16:07:55 +0900

    slight fix

--- ARC/129/A_SmallerXOR.py
+++ ARC/129/A_SmallerXOR.py
@@ -1,10 +1,12 @@
 n, l, r = map(int, input().split())
+
 k = len(bin(n)) - 2
 ans = 0
 for i in range(k):
-    if n>>i & 1:
-        kl = 1<<i
-        kr = (1<<i + 1) - 1
-        if kr < l or kl > r: continue
+    if n >> i & 1:
+        kl = 1 << i
+        kr = (1 << i + 1) - 1
+        if kr < l or kl > r:
+            continue
         ans += min(r, kr) - max(l, kl) + 1
 print(ans)
