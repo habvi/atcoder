5be8104a066969a582bf0a0f430f3931b731bc3b
habvi <a2231250601h@yahoo.co.jp>
2022-02-14 01:53:10 +0900

    refactor

--- ARC/135/B_Sum-of-ThreeTerms.py
+++ ARC/135/B_Sum-of-ThreeTerms.py
@@ -10,31 +10,28 @@ A = [0, 0, 0]
 for i in range(n - 1):
     A.append((A[i] + S[i + 1] - S[i]))
 
-m1 = min(A[i] for i in range(0, n + 2, 3))
-m2 = min(A[i] for i in range(1, n + 2, 3))
-m3 = min(A[i] for i in range(2, n + 2, 3))
 
-if m1 < 0:
-    for i in range(0, n + 2, 3):
-        A[i] -= m1
-if m2 < 0:
-    for i in range(1, n + 2, 3):
-        A[i] -= m2
-if m3 < 0:
-    for i in range(2, n + 2, 3):
-        A[i] -= m3
+def raise_bottom(start):
+    mn = min(A[i] for i in range(start, n + 2, 3))
+    if mn < 0:
+        for i in range(start, n + 2, 3):
+            A[i] -= mn
+
+
+for i in range(3):
+    raise_bottom(i)
 
 s3 = sum(A[:3])
-if s3 > S[0]:
+plus = S[0] - s3
+if plus < 0:
     print('No')
     exit()
 
-plus = S[0] - s3
 for i in range(0, n + 2, 3):
     A[i] += plus
 
 for i in range(n):
-    if sum(A[i : i + 3]) != S[i]:
+    if sum(A[i : i+3]) != S[i]:
         print('No')
         exit()
 
