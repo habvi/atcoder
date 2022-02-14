06951f0ad8eca884d0c3cf465f68a3f53d92f28f
habvi <a2231250601h@yahoo.co.jp>
2022-02-14 22:59:32 +0900

    fix: indent

--- ABC/185/E_SequenceMatching.py
+++ ABC/185/E_SequenceMatching.py
@@ -1,18 +1,21 @@
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
 dp = [[float('inf')] * (m + 1) for _ in range(n + 1)]
 dp[0][0] = 0
+
 for i in range(n + 1):
     for j in range(m + 1):
         if i < n and j < m:
-            if a[i] == b[j]:
+            if A[i] == B[j]:
                 dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j])
             else:
                 dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + 1)
+
         if i < n:
             dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
         if j < m:
             dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
+
 print(dp[n][m])
