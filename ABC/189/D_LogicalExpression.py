88a7b03a189db5833428da30390e1be7bffd1265
habvi <a2231250601h@yahoo.co.jp>
2022-02-14 22:35:23 +0900

    fix: indent

--- ABC/189/D_LogicalExpression.py
+++ ABC/189/D_LogicalExpression.py
@@ -1,7 +1,9 @@
 n = int(input())
+
 dp = [[0] * 2 for i in range(n + 1)]
 dp[0][0] = 1
 dp[0][1] = 1
+
 for i in range(n):
     s = input()
     if s == 'AND':
@@ -14,4 +16,5 @@ for i in range(n):
         dp[i + 1][0 | 1] += dp[i][1]
         dp[i + 1][1 | 0] += dp[i][0]
         dp[i + 1][1 | 1] += dp[i][1]
+
 print(dp[n][1])
