bbc3b9c0d675534fdca29bef2055788e9bf9faf5
habvi <a2231250601h@yahoo.co.jp>
2022-02-16 00:05:56 +0900

    slight fix

--- ABC/106/B_105.py
+++ ABC/106/B_105.py
@@ -1,14 +1,17 @@
 def div_lis(x):
     div_l, div_r = [], []
-    for i in range(1, int(x**0.5)+1):
-        if x % i != 0: continue
+    for i in range(1, int(x ** 0.5) + 1):
+        if x % i != 0:
+            continue
         div_l.append(i)
         if i != x // i:
             div_r.append(x // i)
     div = div_l + div_r[::-1]
     return div
 
+
 n = int(input())
+
 ans = 0
 for i in range(1, n + 1, 2):
     if len(div_lis(i)) == 8:
