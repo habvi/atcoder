3e9ce313459fb44457a73cae6e6f4e59fe4cd471
habvi <a2231250601h@yahoo.co.jp>
2022-02-17 00:42:23 +0900

    add: 074_D

---
+++ ABC/074/D_RestoringRoadNetwork.py
@@ -0,0 +1,30 @@
+n = int(input())
+dst = [list(map(int, input().split())) for _ in range(n)]
+
+edges = []
+for i in range(n):
+    for j in range(i + 1, n):
+        if i == j:
+            continue
+        edges.append((dst[i][j], i, j))
+
+erase = set()
+for d, i, j in edges:
+    for k in range(n):
+        if i == k or k == j:
+            continue
+
+        if d > dst[i][k] + dst[k][j]:
+            print(-1)
+            exit()
+
+        if d == dst[i][k] + dst[k][j]:
+            erase.add((i, j))
+
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if (i, j) in erase:
+            continue
+        ans += dst[i][j]
+print(ans)
