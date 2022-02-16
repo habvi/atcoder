0eff5b7b220e904e9822be18d288160cc339e9ca
habvi <a2231250601h@yahoo.co.jp>
2022-02-17 01:56:40 +0900

    refactor

--- ARC/116/C_MultipleSequences.py
+++ ARC/116/C_MultipleSequences.py
@@ -3,10 +3,12 @@ from collections import defaultdict
 def Sieve(x):
     memo = [0] * (x + 1)
     for i in range(2, x + 1):
-        if memo[i]: continue
+        if memo[i]:
+            continue
         memo[i] = i
         for j in range(i * i, x + 1, i):
-            if memo[j]: continue
+            if memo[j]:
+                continue
             memo[j] = i
     return memo
 
@@ -17,13 +19,11 @@ def comb(n, r, MOD):
     dnmnt = invfact[n - r] * invfact[r] % MOD
     return nmrt * dnmnt % MOD
 
+
 n, m = map(int, input().split())
 MOD = 998244353
-mx = 3 * 10**5
 
-memo = Sieve(mx)
-
-lenc = mx
+lenc = 3 * 10**5
 fact, invfact = [1] * lenc, [1] * lenc
 for i in range(1, lenc):
     fact[i] = fact[i - 1] * i % MOD
@@ -32,17 +32,22 @@ invfact[lenc - 1] = pow(fact[lenc - 1], MOD - 2, MOD)
 for i in range(lenc - 1, 0, -1):
     invfact[i-1] = invfact[i] * i % MOD
 
+memo = Sieve(m)
+
 ans = 0
 for i in range(1, m + 1):
-    d = defaultdict(int)
-    while i > 1:
-        d[memo[i]] += 1
-        i //= memo[i]
-    
-    tot = 1
-    for k, v in d.items():
-        tot *= comb(n + v - 1, v, MOD)
-        tot %= MOD
-    ans += tot
+    pfact = defaultdict(int)
+    rev = i
+    while rev > 1:
+        pfact[memo[rev]] += 1
+        rev //= memo[rev]
+
+    total = 1
+    for v in pfact.values():
+        total *= comb(n - 1 + v, v, MOD)
+        total %= MOD
+
+    ans += total
     ans %= MOD
+
 print(ans)
