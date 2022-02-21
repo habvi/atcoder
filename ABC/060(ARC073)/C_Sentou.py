85edcda8462085aeb780cdf87db822c8d0de17b1
habvi <a2231250601h@yahoo.co.jp>
2022-02-21 10:04:29 +0900

    add: 060_C, rename dir

---
+++ ABC/060(ARC073)/C_Sentou.py
@@ -0,0 +1,13 @@
+n, T = map(int, input().split())
+A = [*map(int, input().split()), float('inf')]
+
+ans = 0
+now = 0
+for a in A:
+    if a < now + T:
+        ans += a - now
+    else:
+        ans += T
+    now = a
+
+print(ans)
