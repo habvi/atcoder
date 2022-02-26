ba8d762ea49f697680d4ebbf04a5e78133e91364
habvi <a2231250601h@yahoo.co.jp>
2022-02-27 01:47:11 +0900

    add: 241_B

---
+++ ABC/241/B_Pasta.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B =list(map(int, input().split()))
+
+c = Counter(A)
+for b in B:
+    if c[b] == 0:
+        print('No')
+        exit()
+    c[b] -= 1
+
+print('Yes')
