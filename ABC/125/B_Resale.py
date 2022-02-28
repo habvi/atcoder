0e0338c95b131bbeaea3788bb4a823f0d5764434
habvi <a2231250601h@yahoo.co.jp>
2022-02-28 23:37:42 +0900

    refactor: a little

--- ABC/125/B_Resale.py
+++ ABC/125/B_Resale.py
@@ -1,9 +1,9 @@
 n = int(input())
-v = list(map(int, input().split()))
-c = list(map(int, input().split()))
+V = list(map(int, input().split()))
+C = list(map(int, input().split()))
 
 ans = 0
-for i in range(n):
-    if v[i] > c[i]:
-        ans += v[i] - c[i]
+for v, c in zip(V, C):
+    if v > c:
+        ans += v - c
 print(ans)
