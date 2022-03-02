4d5bf2b910d491c38e2086dc03b002fe8ea38c54
habvi <a2231250601h@yahoo.co.jp>
2022-03-02 22:05:24 +0900

    slight refactor

--- ABC/233/C_Product.py
+++ ABC/233/C_Product.py
@@ -1,15 +1,16 @@
 from itertools import product
+
 n, x = map(int, input().split())
 A = []
 for _ in range(n):
-    l, *a = map(int, input().split())
+    L, *a = map(int, input().split())
     A.append(a)
 
 ans = 0
 for pr in product(*A):
-    a = 1
+    mul = 1
     for p in pr:
-        a *= p
-    if a == x:
-        ans += 1
+        mul *= p
+    ans += mul == x
+
 print(ans)
