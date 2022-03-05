f94c2c013d36ad6cb151c9b6b4cae184bc7b2612
habvi <a2231250601h@yahoo.co.jp>
2022-03-05 15:57:46 +0900

    refactor

--- ABC/227/B_KEYENCEbuilding.py
+++ ABC/227/B_KEYENCEbuilding.py
@@ -1,27 +1,12 @@
 n = int(input())
-s = list(map(int, input().split()))
-cnt = 0
-cr = [0] * n
-for i in range(1, 1005):
-    for j in range(1, 1005):
-        a = 4*i*j + 3*i + 3*j
-        if a > 1000: continue
+A = list(map(int, input().split()))
 
-        for k in range(n):
-            if a == s[k]:
-                cr[k] = 1
-print(n - sum(cr))
+s = set()
+for i in range(1, 1001):
+    for j in range(1, 1001):
+        s.add(4*i*j + 3*i + 3*j)
 
-
-
-# set() あまり速度変わらず
-# n = int(input())
-# s = list(map(int, input().split()))
-# cnt = 0
-# st = set()
-# for i in range(1, 1005):
-#     for j in range(1, 1005):
-#         a = 4*i*j + 3*i + 3*j
-#         if a > 1000: continue
-#         st.add(a)
-# print(n - sum(1 for i in s if i in st))
+ans = 0
+for a in A:
+    ans += a not in s
+print(ans)
