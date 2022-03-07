e285fbeff950d0e8c78a456885f0e087539afc21
habvi <a2231250601h@yahoo.co.jp>
2022-03-08 00:10:48 +0900

    add: log2

--- ABC/136/D_GatheringChildren_doubling.py
+++ ABC/136/D_GatheringChildren_doubling.py
@@ -1,15 +1,19 @@
+from math import log2
+
 S = input()
 n = len(S)
 
-doubling = [[0] * n for _ in range(18)]
+m = int(log2(10 ** 5)) + 1
+
+doubling = [[0] * n for _ in range(m + 1)]
 for i, s in enumerate(S):
     doubling[0][i] = (i + 1 if s == 'R' else i - 1)
 
-for k in range(17):
+for k in range(m):
     for i in range(n):
         doubling[k + 1][i] = doubling[k][doubling[k][i]]
 
 ans = [0] * n
-for num in doubling[17]:
+for num in doubling[m]:
     ans[num] += 1
 print(*ans)
