7234c69697140d5a0b5a85bbf25ab67adf262a0a
habvi <a2231250601h@yahoo.co.jp>
2022-03-08 19:40:31 +0900

    add: 179_D

---
+++ ABC/179/D_LeapingTak.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+lr = [tuple(map(int, input().split())) for _ in range(k)]
+MOD = 998244353
+
+dp = [0] * (n + 1)
+dp[1] = 1
+
+for i in range(2, n + 1):
+    total = 0
+    for L, R in lr:
+        R += 1
+        l = max(0, i - R)
+        r = max(0, i - L)
+        total += dp[r] - dp[l]
+
+    dp[i] = dp[i - 1] + total
+    dp[i] %= MOD
+ 
+print((dp[-1] - dp[-2] + MOD) % MOD)
