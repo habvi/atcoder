9b6165af507672b09f066ef8868f7f2e2b5da239
habvi <a2231250601h@yahoo.co.jp>
2022-03-13 01:50:29 +0900

    add: 243_A

---
+++ ABC/243/A_Shampoo.py
@@ -0,0 +1,17 @@
+v, a, b, c = map(int, input().split())
+
+while True:
+    if v - a < 0:
+        print('F')
+        exit()
+    v -= a
+
+    if v - b < 0:
+        print('M')
+        exit()
+    v -= b
+
+    if v - c < 0:
+        print('T')
+        exit()
+    v -= c
