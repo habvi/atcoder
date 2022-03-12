958c3d07e64c56b0b3ba2a356c3c674366d3dc43
habvi <a2231250601h@yahoo.co.jp>
2022-03-12 18:33:41 +0900

    add: 190_D

---
+++ ABC/190/D_StaircaseSequences.py
@@ -0,0 +1,20 @@
+def div_lis(x):
+    div_l, div_r = [], []
+    for i in range(1, int(x**0.5) + 1):
+        if x % i != 0:
+            continue
+        div_l.append(i)
+        if i != x // i:
+            div_r.append(x // i)
+    div = div_l + div_r[::-1]
+    return div
+
+
+n = int(input())
+
+ans = 0
+n2 = n * 2
+for d in div_lis(n2):
+    if d % 2 != n2 // d % 2:
+        ans += 1
+print(ans)
