0f6f9fb8d3f6a8065e341804e11064f0480b5ede
habvi <a2231250601h@yahoo.co.jp>
2022-03-14 17:29:34 +0900

    refactor

--- ABC/240/F_SumSumMax.py
+++ ABC/240/F_SumSumMax.py
@@ -1,39 +1,27 @@
 def sigma(x, n):
-    return x * n*(n - 1)//2
-
-def is_ok(y, pre, x):
-    return pre + x*y > 0 
-
-def bisect(ng, ok, pre, x):
-    while abs(ok - ng) > 1:
-        mid = (ok + ng) // 2
-        if is_ok(mid, pre, x):
-            ok = mid
-        else:
-            ng = mid
-    return ok
-
+    return x * n*(n + 1)//2
 
 def solve():
     n, _ = map(int, input().split())
 
-    B = [0, 0]
+    B = [0]
     A = [0]
     ans = -float('inf')
-    for i in range(n):
+    for _ in range(n):
         x, y = map(int, input().split())
+        a, b = A[-1], B[-1]
 
-        a_left = A[-1] + B[i + 1] + x
-        a_right = A[-1] + (B[i + 1] + x)*y + sigma(x, y)
+        a_left = a + b + x
+        a_right = a + b*y + sigma(x, y)
         ans = max(ans, a_left, a_right)
 
-        if x < 0 and B[-1] > 0:
-            ny = bisect(y + 1, 0, B[-1], x)
+        if x < 0 and b > 0:
+            ny = b // -x
             if 0 < ny < y:
-                na = A[-1] + (B[i + 1] + x)*ny + sigma(x, ny)
+                na = a + b*ny + sigma(x, ny)
                 ans = max(ans, na)
 
-        B.append(B[-1] + x*y)
+        B.append(b + x*y)
         A.append(a_right)
     return ans
 
