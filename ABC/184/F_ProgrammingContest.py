8ce8732665e08b8e226b92ffaf117e8b18f218d9
habvi <a2231250601h@yahoo.co.jp>
2022-03-14 22:02:15 +0900

    slight refactor

--- ABC/184/F_ProgrammingContest.py
+++ ABC/184/F_ProgrammingContest.py
@@ -1,29 +1,30 @@
 from bisect import bisect
 
-def get_each_sum(A):
-    sm = []
-    n = len(A)
-    for bit in range(1 << n):
-        tot = 0
-        for i in range(n):
+def times(A):
+    res = []
+    m = len(A)
+    for bit in range(1 << m):
+        time_ = 0
+        for i in range(m):
             if bit >> i & 1:
-                tot += A[i]
-        sm.append(tot)
-    return sm
-        
+                time_ += A[i]
+        res.append(time_)
+    return res
+
 
 n, t = map(int, input().split())
 A = list(map(int, input().split()))
-mid = n // 2
 
-a1 = get_each_sum(A[:mid])
-a2 = get_each_sum(A[mid:])
-a2.sort()
+mid = n // 2
+left = times(A[:mid])
+right = times(A[mid:])
+right.sort()
 
 ans = 0
-for a in a1:
-    bi = bisect(a2, t - a)
-    if not bi:
+for num in left:
+    if num >= t:
         continue
-    ans = max(ans, a + a2[bi - 1])
+    bi = bisect(right, t - num)
+    ans = max(ans, num + right[bi - 1])
+
 print(ans)
