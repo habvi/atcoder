ade8f43574e2a9b21ed85c041334c3dbead383f8
habvi <a2231250601h@yahoo.co.jp>
2022-03-18 23:59:06 +0900

    refactor

--- ABC/120/D_DecayedBridges.py
+++ ABC/120/D_DecayedBridges.py
@@ -1,29 +1,36 @@
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10 ** 7)
+
 def root(x):
-    if rank[x] < 0: return x
+    if rank[x] < 0:
+       return x
     rank[x] = root(rank[x])
     return rank[x]
 def unite(x, y):
     x, y = root(x), root(y)
-    if x == y: return False
-    if rank[x] > rank[y]: x, y = y, x
+    if x == y:
+        return False
+    if rank[x] > rank[y]:
+        x, y = y, x
     rank[x] += rank[y]
     rank[y] = x
     return True
-def is_same(x, y): return root(x) == root(y)
-def size(x): return -rank[root(x)]
+def is_same(x, y):
+    return root(x) == root(y)
+def size(x):
+    return -rank[root(x)]
+
 
 n, m = map(int, input().split())
-q = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(m)]
-rank = [-1] * n
-tot = (n - 1) * n // 2
-ans = [tot]
-for a, b in reversed(q[1:]):
-    if not is_same(a, b):
-        tot -= size(a) * size(b)
-        unite(a, b)
-    ans.append(tot)
+ab = [tuple(map(lambda x: int(x)- 1, input().split())) for _ in range(m)][::-1]
 
-for a in ans[::-1]:
-    print(a)
+rank = [-1] * n
+total = n * (n - 1) // 2
+ans = [total]
+for a, b in ab:
+    if not is_same(a, b):
+        total -= size(a) * size(b)
+        unite(a, b)
+    ans.append(total)
+
+print(*ans[::-1][1:])
