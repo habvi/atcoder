1a67854b3bcb28a5a77976e513524b50c4cbaca2
habvi <a2231250601h@yahoo.co.jp>
2022-03-18 23:38:32 +0900

    refactor

--- ABC/213/C_ReorderCards.py
+++ ABC/213/C_ReorderCards.py
@@ -1,29 +1,14 @@
 h, w, n = map(int, input().split())
-ab = [tuple(map(int, input().split())) for _ in range(n)]
-
-idx = {}
-for i, (a, b) in enumerate(ab):
-    idx[(a, b)] = i
-
-ans_x = [None] * n
-x = [(0, 0), *sorted(ab, key=lambda x: x[1])]
-dst = 0
+y, x = set(), set()
+ab = []
 for i in range(n):
-    l = x[i][1]
-    r = x[i + 1][1]
-    dst += max(0, r - l - 1)
-    a, b = x[i + 1]
-    ans_x[idx[(a, b)]] = r - dst
+    a, b = map(int, input().split())
+    y.add(a)
+    x.add(b)
+    ab.append((a, b))
 
-ans_y = [None] * n
-y = [(0, 0), *sorted(ab, key=lambda x: x[0])]
-dst = 0
-for i in range(n):
-    l = y[i][0]
-    r = y[i + 1][0]
-    dst += max(0, r - l - 1)
-    a, b = y[i + 1]
-    ans_y[idx[(a, b)]] = r - dst
+Y = {num : i for i, num in enumerate(sorted(y), 1)}
+X = {num : i for i, num in enumerate(sorted(x), 1)}
 
-for y, x in zip(ans_y, ans_x):
-    print(y, x)
+for a, b in ab:
+    print(Y[a], X[b])
