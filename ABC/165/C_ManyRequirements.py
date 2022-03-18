85e4226135a7cb8db0e023c5894678e115f71b70
habvi <a2231250601h@yahoo.co.jp>
2022-03-19 01:07:28 +0900

    refactor

--- ABC/165/C_ManyRequirements.py
+++ ABC/165/C_ManyRequirements.py
@@ -1,15 +1,14 @@
 from itertools import combinations_with_replacement
+
 n, m, q = map(int, input().split())
 A = [tuple(map(int, input().split())) for _ in range(q)]
-s = set()
-for com in combinations_with_replacement(range(1, m + 1), n):
-    s.add(tuple(sorted(com)))
 
 ans = 0
-for p in s:
-    tot = 0
-    for a, b, c, d in A:
-        if p[b - 1] - p[a - 1] == c:
-            tot += d
-    ans = max(ans, tot)
+for com in combinations_with_replacement(range(1, m + 1), n):
+    total = 0
+    for a, b, c, d  in A:
+        a, b = a - 1, b - 1
+        total += (d if com[b] - com[a] == c else 0)
+
+    ans = max(ans, total)
 print(ans)
