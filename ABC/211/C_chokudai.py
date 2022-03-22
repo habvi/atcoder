94dd300565330db7e4a81646760ddb9ad8584166
habvi <a2231250601h@yahoo.co.jp>
2022-03-22 23:53:16 +0900

    add: 211_C DP

--- ABC/211/C_chokudai.py
+++ ABC/211/C_chokudai.py
@@ -1,18 +1,14 @@
-s = input()
-lens = len(s)
-w = 'chokudai'
-lenw = len(w)
-MOD = 10**9+7
-dp = [[0]*(lens+1) for _ in range(lenw+1)]
-for i in range(lens+1):
-    dp[0][i] = 1
+S = input()
+MOD = 10**9 + 7
+WORD = 'chokudai'
 
-for i in range(1, lenw+1):
-    for j in range(1, lens+1):
-        dp[i][j] += dp[i][j-1]
-        dp[i][j] %= MOD
-        if s[j-1] not in w: continue
-        if w.index(s[j-1]) == i-1:
-            dp[i][j] += dp[i-1][j-1]
-            dp[i][j] %= MOD
-print(dp[lenw][lens])
+dp = [0] * 9
+dp[0] = 1
+
+for s in S:
+    for i, w in enumerate(WORD, 1):
+        if s == w:
+            dp[i] += dp[i - 1]
+            dp[i] %= MOD
+
+print(dp[-1])
