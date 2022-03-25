ff316cb0115d2f87f0e44605e679a3c37b9f3789
habvi <a2231250601h@yahoo.co.jp>
2022-03-26 00:09:09 +0900

    refactor

--- ABC/076/C_DubiousDocument2.py
+++ ABC/076/C_DubiousDocument2.py
@@ -1,25 +1,23 @@
-s = input()
-t = input()
-ng = 'UNRESTORABLE'
-ls = len(s)
-lt = len(t)
+S = input()
+T = input()
+n = len(S)
+m = len(T)
+NG = 'UNRESTORABLE'
 
-lis = []
-for i in range(ls - lt + 1):
-    ss = s[i : i+lt]
-    for a, b in zip(ss, t):
-        if a not in ('?', b):
-            break
-    else:
-        ans = []
-        for j in range(i):
-            ans.append('a' if s[j] == '?' else s[j])
-            
-        ans.extend(list(t))
-        
-        for j in range(i + lt, ls):
-            ans.append('a' if s[j] == '?' else s[j])
-        lis.append("".join(ans))
-        
-lis.sort()
-print(lis[0] if lis else ng)
+cand = []
+for i in reversed(range(n - m + 1)):
+    ok = True
+    for s, t in zip(S[i : i + m], T):
+        if s == t or s == '?':
+            continue
+        ok = False
+        break
+
+    if ok:
+        C = list(S)
+        C[i : i + m] = T
+        C = ['a' if c == '?' else c for c in C]
+        cand.append(''.join(C))
+
+cand.sort()
+print(cand[0] if cand else NG)
