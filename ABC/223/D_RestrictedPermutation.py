b64b1b98e01700317d47743ae917b20ebf93ad36
habvi <a2231250601h@yahoo.co.jp>
2022-03-27 17:24:33 +0900

    refactor

--- ABC/223/D_RestrictedPermutation.py
+++ ABC/223/D_RestrictedPermutation.py
@@ -1,17 +1,17 @@
+from heapq import heappop, heappush
 from collections import defaultdict
-from heapq import heapify, heappop, heappush
+
 n, m = map(int, input().split())
 g = defaultdict(list)
-cnt = [0] * n
+in_deg = defaultdict(int)
 for _ in range(m):
     a, b = map(lambda x: int(x) - 1, input().split())
     g[a].append(b)
-    cnt[b] += 1
+    in_deg[b] += 1
 
 hq = []
-heapify(hq)
 for i in range(n):
-    if not cnt[i]:
+    if not in_deg[i]:
         heappush(hq, i)
 
 ans = []
@@ -19,11 +19,8 @@ while hq:
     v = heappop(hq)
     ans.append(v + 1)
     for nv in g[v]:
-        cnt[nv] -= 1
-        if not cnt[nv]:
+        in_deg[nv] -= 1
+        if not in_deg[nv]:
             heappush(hq, nv)
 
-if len(ans) == n:
-    print(*ans)
-else:
-    print(-1)
+print(*ans if len(ans) == n else [-1])
