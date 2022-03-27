87d47e0fc4c953b94f2a35059b11e8e452fbd310
habvi <a2231250601h@yahoo.co.jp>
2022-03-27 14:31:07 +0900

    functionalize

--- ABC/245/F_EndlessWalk_BFS.py
+++ ABC/245/F_EndlessWalk_BFS.py
@@ -2,30 +2,50 @@ from collections import deque, defaultdict
 import sys
 sys.setrecursionlimit(10 ** 7)
 
-def dfs1(v):
-    seen1[v] = 1
-    for nv in g[v]:
-        if seen1[nv]:
-            continue
-        dfs1(nv)
-    route.append(v)
+def make_scc(g, rg):
+    def dfs1(v):
+        seen1[v] = 1
+        for nv in g[v]:
+            if seen1[nv]:
+                continue
+            dfs1(nv)
+        route.append(v)
 
-def dfs2(v):
-    seen2[v] = 1
-    scc.append(v)
-    for nv in rg[v]:
-        if seen2[nv]:
+    def dfs2(v):
+        seen2[v] = 1
+        scc_each.append(v)
+        for nv in rg[v]:
+            if seen2[nv]:
+                continue
+            dfs2(nv)
+
+    route = []
+    seen1 = [0] * n
+    for v in range(n):
+        if seen1[v]:
             continue
-        dfs2(nv)
+        dfs1(v)
+
+    seen2 = [0] * n
+    scc = []
+    for v in route[::-1]:
+        if seen2[v]:
+            continue
+        scc_each = []
+        dfs2(v)
+        scc.append(scc_each)
+
+    return scc
+
 
 def bfs(u):
     q = deque([u])
     while q:
         v = q.popleft()
         for nv in rg[v]:
-            if seen3[nv]:
+            if seen[nv]:
                 continue
-            seen3[nv] = 1
+            seen[nv] = 1
             q.append(nv)
 
 
@@ -37,30 +57,18 @@ for _ in range(m):
     g[a].append(b)
     rg[b].append(a)
 
-route = []
-seen1 = [0] * n
-for v in range(n):
-    if seen1[v]:
+cycle = []
+scc = make_scc(g, rg)
+for v in scc:
+    if len(v) == 1:
         continue
-    dfs1(v)
+    cycle.extend(v)
 
-seen2 = [0] * n
-cycle = set()
-for v in route[::-1]:
-    if seen2[v]:
-        continue
-
-    scc = []
-    dfs2(v)
-    if len(scc) >= 2:
-        for v in scc:
-            cycle.add(v)
-
-seen3 = [0] * n
+seen = [0] * n
 for v in cycle:
-    if seen3[v]:
+    if seen[v]:
         continue
-    seen3[v] = 1
+    seen[v] = 1
     bfs(v)
 
-print(sum(seen3))
+print(sum(seen))
