835c125c7f8a99d535c37b5b3de9f62eae351d68
habvi <a2231250601h@yahoo.co.jp>
2022-03-27 23:37:03 +0900

    refactor

--- ABC/022/C_BlueBird.py
+++ ABC/022/C_BlueBird.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from itertools import combinations
 
 n, m = map(int, input().split())
@@ -8,13 +7,12 @@ dist = [[INF] * n for _ in range(n)]
 for i in range(n):
     dist[i][i] = 0
 
-g = defaultdict(list)
+g = []
 for _ in range(m):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    if a == 0 or b == 0:
-        g[a].append((b, c))
-        g[b].append((a, c))
+    if a == 0:
+        g.append((b, c))
         continue
     dist[a][b] = c
     dist[b][a] = c
@@ -25,7 +23,7 @@ for k in range(n):
             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 ans = INF
-for (v1, c1), (v2, c2) in combinations(g[0], 2):
+for (v1, c1), (v2, c2) in combinations(g, 2):
     total = c1 + c2 + dist[v1][v2]
     ans = min(ans, total)
 
