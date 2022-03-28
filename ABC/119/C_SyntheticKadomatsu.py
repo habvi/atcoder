8aa229d691c8b1a76986b8f7b2e007822e37136b
habvi <a2231250601h@yahoo.co.jp>
2022-03-28 14:47:02 +0900

    refactor

--- ABC/119/C_SyntheticKadomatsu.py
+++ ABC/119/C_SyntheticKadomatsu.py
@@ -1,20 +1,18 @@
 from itertools import product
-from collections import Counter
 
 n, A, B, C = map(int, input().split())
 L = [int(input()) for _ in range(n)]
 
 ans = float('inf')
 for pr in product(range(4), repeat=n):
-    c = Counter(pr)
-    if not c[0] or not c[1] or not c[2]:
-        continue
-
     bamboo = [[] for _ in range(3)]
     for i in range(n):
         if pr[i] == 3:
             continue
         bamboo[pr[i]].append(L[i])
+
+    if not bamboo[0] or not bamboo[1] or not bamboo[2]:
+        continue
 
     mp = 0
     for each, target in zip(bamboo, (A, B, C)):
