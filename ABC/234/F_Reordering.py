2bf2dff5a33e17a2514ba2af346e693e81a6b7ab
habvi <a2231250601h@yahoo.co.jp>
2022-03-29 13:35:17 +0900

    slight refactor

--- ABC/234/F_Reordering.py
+++ ABC/234/F_Reordering.py
@@ -1,13 +1,16 @@
 from collections import Counter
+
 def comb(n, r, MOD):
     if n < r or n < 0 or r < 0:
         return 0
+    r = min(r, n - r)
     nmrt = fact[n]
     dnmnt = invfact[n - r] * invfact[r] % MOD
     return nmrt * dnmnt % MOD
 
-s = input()
-n = len(s)
+
+S = input()
+n = len(S)
 MOD = 998244353
 
 lenc = n + 5
@@ -19,20 +22,17 @@ invfact[lenc - 1] = pow(fact[lenc - 1], MOD - 2, MOD)
 for i in range(lenc - 1, 0, -1):
     invfact[i-1] = invfact[i] * i % MOD
 
-c = Counter(s)
+
 dp = [0] * (n + 1)
 dp[0] = 1
-for _, v in c.items():
+for _, num in Counter(S).items():
     ndp = [0] * (n + 1)
-    for k in range(v + 1):
-        for i in range(n - k + 1):
-            j = i + k
-            ndp[j] += dp[i] * comb(j, i, MOD)
-            ndp[j] %= MOD
+    for new in range(num + 1):
+        for pre in range(n + 1):
+            total = pre + new
+            if total <= n:
+                ndp[total] += dp[pre] * comb(total, new, MOD)
+                ndp[total] %= MOD
     dp = ndp
 
-ans = 0
-for i in range(1, n + 1):
-    ans += dp[i]
-    ans %= MOD
-print(ans)
+print((sum(dp) - 1 + MOD) % MOD)
