a6387b54cce1e1231df2d6b341950f567413a9aa
habvi <a2231250601h@yahoo.co.jp>
2022-03-30 23:59:42 +0900

    slight refactor

--- ABC/235/C_TheKthTimeQuery.py
+++ ABC/235/C_TheKthTimeQuery.py
@@ -1,14 +1,13 @@
 from collections import defaultdict
-n, q = map(int, input().split())
-A = list(map(int, input().split()))
-d = defaultdict(list)
-for i, a in enumerate(A, 1):
-    d[a].append(i)
 
-for _ in range(q):
+n, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+times = defaultdict(list)
+for i, a in enumerate(A):
+    times[a].append(i + 1)
+
+for _ in range(Q):
     x, k = map(int, input().split())
     k -= 1
-    if len(d[x]) <= k:
-        print(-1)
-    else:
-        print(d[x][k])
+    print(times[x][k] if len(times[x]) > k else -1) 
