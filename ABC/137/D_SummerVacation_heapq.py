68cf8b79bbf907029d1af2056f740709497023d1
habvi <a2231250601h@yahoo.co.jp>
2022-03-30 19:23:30 +0900

    refactor

--- ABC/137/D_SummerVacation_heapq.py
+++ ABC/137/D_SummerVacation_heapq.py
@@ -1,20 +1,20 @@
-from heapq import heapify, heappop, heappush
+from heapq import heappop, heappush
 from collections import defaultdict
+
 n, m = map(int, input().split())
-day = defaultdict(list)
+ab = defaultdict(list)
 for _ in range(n):
     a, b = map(int, input().split())
     if a > m:
         continue
-    day[a].append(b)
+    ab[a].append(b)
 
 hq = []
-heapify(hq)
 ans = 0
 for i in range(1, m + 1):
-    if i in day:
-        for d in day[i]:
-            heappush(hq, -d)
+    for b in ab[i]:
+        heappush(hq, -b)
+
     if hq:
-        ans += heappop(hq)
-print(-ans)
+        ans += -heappop(hq)
+print(ans)
