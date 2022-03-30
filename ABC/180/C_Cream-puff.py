ff852eedc0b56b96aefb7bf63ec8ca986488c46e
habvi <a2231250601h@yahoo.co.jp>
2022-03-30 23:58:15 +0900

    add: 180_C

---
+++ ABC/180/C_Cream-puff.py
@@ -0,0 +1,16 @@
+def div_lis(x):
+    div_l, div_r = [], []
+    i = 1
+    while i * i <= x:
+        if x % i == 0:
+            div_l.append(i)
+            if i != x // i:
+                div_r.append(x // i)
+        i += 1
+    div = div_l + div_r[::-1]
+    return div
+
+
+n = int(input())
+for ans in div_lis(n):
+    print(ans)
