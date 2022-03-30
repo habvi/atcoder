c509eaac284bcf92ca7dfc3c90c447627b52b0e2
habvi <a2231250601h@yahoo.co.jp>
2022-03-30 18:48:00 +0900

    add: 137_D sortedset ver.

---
+++ ABC/137/D_SummerVacation_Sortedset.py
@@ -0,0 +1,150 @@
+# https://github.com/tatyam-prime/SortedSet/blob/main/SortedSet.py
+import math
+from bisect import bisect_left, bisect_right
+from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+T = TypeVar('T')
+
+class SortedSet(Generic[T]):
+    BUCKET_RATIO = 50
+    REBUILD_RATIO = 170
+
+    def _build(self, a=None) -> None:
+        "Evenly divide `a` into buckets."
+        if a is None: a = list(self)
+        size = self.size = len(a)
+        bucket_size = int(math.ceil(math.sqrt(size / self.BUCKET_RATIO)))
+        self.a = [a[size * day // bucket_size : size * (day + 1) // bucket_size] for day in range(bucket_size)]
+
+    def __init__(self, a: Iterable[T] = []) -> None:
+        "Make a new SortedSet from iterable. / O(N) if sorted and unique / O(N log N)"
+        a = list(a)
+        if not all(a[day] < a[day + 1] for day in range(len(a) - 1)):
+            a = sorted(set(a))
+        self._build(a)
+
+    def __iter__(self) -> Iterator[T]:
+        for day in self.a:
+            for j in day: yield j
+
+    def __reversed__(self) -> Iterator[T]:
+        for day in reversed(self.a):
+            for j in reversed(day): yield j
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __repr__(self) -> str:
+        return "SortedSet" + str(self.a)
+
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
+
+    def _find_bucket(self, x: T) -> List[T]:
+        "Find the bucket which should contain x. self must not be empty."
+        for a in self.a:
+            if x <= a[-1]: return a
+        return a
+
+    def __contains__(self, x: T) -> bool:
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        day = bisect_left(a, x)
+        return day != len(a) and a[day] == x
+
+    def add(self, x: T) -> bool:
+        "Add an element and return True if added. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return True
+        a = self._find_bucket(x)
+        day = bisect_left(a, x)
+        if day != len(a) and a[day] == x: return False
+        a.insert(day, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.REBUILD_RATIO:
+            self._build()
+        return True
+
+    def discard(self, x: T) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        day = bisect_left(a, x)
+        if day == len(a) or a[day] != x: return False
+        a.pop(day)
+        self.size -= 1
+        if len(a) == 0: self._build()
+        return True
+
+    def lt(self, x: T) -> Union[T, None]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
+
+    def le(self, x: T) -> Union[T, None]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: T) -> Union[T, None]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
+
+    def ge(self, x: T) -> Union[T, None]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
+
+    def __getitem__(self, x: int) -> T:
+        "Return the x-th element, or IndexError if it doesn't exist."
+        if x < 0: x += self.size
+        if x < 0: raise IndexError
+        for a in self.a:
+            if x < len(a): return a[x]
+            x -= len(a)
+        raise IndexError
+
+    def index(self, x: T) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x:
+                return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x:
+                return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
+
+
+
+n, m = map(int, input().split())
+ab = [tuple(map(int, input().split())) for _ in range(n)]
+
+ab.sort(key=lambda x: (-x[1], x[0]))
+s = SortedSet(range(m))
+
+ans = 0
+for a, b in ab:
+    if not s:
+        break
+    day = s.le(m - a)
+    if day is None:
+        continue
+    s.discard(day)
+    ans += b
+
+print(ans)
