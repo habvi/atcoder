e1c8ea705797d793b94170b3f01349dac2dc876e
habvi <a2231250601h@yahoo.co.jp>
2022-04-04 01:28:08 +0900

    refactor

--- ABC/176/D_Wizard-in-Maze.py
+++ ABC/176/D_Wizard-in-Maze.py
@@ -1,40 +1,51 @@
 from collections import deque
-def bfs(sy, sx):
-    dst[sy][sx] = 0
-    q = deque([])
-    q.append((sy, sx))
-    while q:
-        y, x = q.popleft()
-        if seen[y][x]:
-            continue
-        seen[y][x] = 1
 
-        for dy, dx in zip((0, 1, 0, -1), (1, 0, -1, 0)):
+DXY = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+def bfs(sy, sx):
+    dist[sy][sx] = 0
+    que = deque([])
+    que.append((sy, sx, 0))
+
+    while que:
+        y, x, c = que.popleft()
+        if (y, x) == (gy, gx):
+            print(dist[gy][gx])
+            exit()
+
+        nc = dist[y][x]
+        if c > nc:
+            continue
+
+        for dy, dx in DXY:
             ny, nx = y + dy, x + dx
-            if not (0 <= ny < h and 0 <= nx < w) or s[ny][nx] == "#":
+            if not (0 <= ny < h and 0 <= nx < w) or S[ny][nx] == '#':
                 continue
-            if dst[ny][nx] <= dst[y][x]:
+            if dist[ny][nx] <= nc:
                 continue
-            dst[ny][nx] = dst[y][x] 
-            q.appendleft((ny, nx))
+            dist[ny][nx] = nc
+            que.appendleft((ny, nx, nc))
 
         for dy in range(-2, 3):
             for dx in range(-2, 3):
                 if dy == dx == 0:
                     continue
                 ny, nx = y + dy, x + dx
-                if not (0 <= ny < h and 0 <= nx < w) or s[ny][nx] == "#":
+                if not (0 <= ny < h and 0 <= nx < w) or S[ny][nx] == '#':
                     continue
-                if dst[ny][nx] <= dst[y][x] + 1:
+                if dist[ny][nx] <= nc + 1:
                     continue
-                dst[ny][nx] = dst[y][x] + 1
-                q.append((ny, nx))
+                dist[ny][nx] = nc + 1
+                que.append((ny, nx, nc + 1))
+
 
 h, w = map(int, input().split())
 sy, sx = map(lambda x: int(x) - 1, input().split())
 gy, gx = map(lambda x: int(x) - 1, input().split())
-s = [input() for _ in range(h)]
-dst = [[float('inf')] * w for _ in range(h)]
-seen = [[0] * w for _ in range(h)]
+S = [input() for _ in range(h)]
+
+INF = float('inf')
+dist = [[INF] * w for _ in range(h)]
 bfs(sy, sx)
-print(dst[gy][gx] if dst[gy][gx] != float('inf') else -1)
+
+print(-1)
--- ABC/213/E_StrongerTakahashi.py
+++ ABC/213/E_StrongerTakahashi.py
@@ -1,20 +1,30 @@
 from collections import deque
 
+DXY = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
 def bfs(sy, sx):
     dist[sy][sx] = 0
-    q = deque([])
-    q.append((sy, sx))
-    while q:
-        y, x = q.popleft()
-        d = dist[y][x]
-        for dy, dx in zip((0, 1, 0, -1), (1, 0, -1, 0)):
+    que = deque([])
+    que.append((sy, sx, 0))
+
+    while que:
+        y, x, c = que.popleft()
+        if (y, x) == (h - 1, w - 1):
+            print(dist[h - 1][w - 1])
+            exit()
+
+        nc = dist[y][x]
+        if c > nc:
+            continue
+
+        for dy, dx in DXY:
             ny, nx = y + dy, x + dx
             if not (0 <= ny < h and 0 <= nx < w) or S[ny][nx] == '#':
                 continue
-            if dist[ny][nx] <= d:
+            if dist[ny][nx] <= nc:
                 continue
-            dist[ny][nx] = d
-            q.appendleft((ny, nx))
+            dist[ny][nx] = nc
+            que.appendleft((ny, nx, nc))
 
         for dy in range(-2, 3):
             for dx in range(-2, 3):
@@ -23,10 +33,10 @@ def bfs(sy, sx):
                 ny, nx = y + dy, x + dx
                 if not (0 <= ny < h and 0 <= nx < w):
                     continue
-                if dist[ny][nx] <= d + 1:
+                if dist[ny][nx] <= nc + 1:
                     continue
-                dist[ny][nx] = d + 1
-                q.append((ny, nx))
+                dist[ny][nx] = nc + 1
+                que.append((ny, nx, nc + 1))
 
 
 h, w = map(int, input().split())
@@ -35,5 +45,3 @@ S = [input() for _ in range(h)]
 INF = float('inf')
 dist = [[INF] * w for _ in range(h)]
 bfs(0, 0)
-
-print(dist[h - 1][w - 1])
