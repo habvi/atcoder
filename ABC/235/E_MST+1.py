c821e675e2dfa56fa8b187a7c059d52b9378de52
habvi <a2231250601h@yahoo.co.jp>
2022-04-05 22:44:41 +0900

    refactor

--- ABC/235/E_MST+1.py
+++ ABC/235/E_MST+1.py
@@ -1,41 +1,49 @@
-from heapq import heapify, heappop, heappush
 import sys
 sys.setrecursionlimit(10 ** 7)
+
 def root(x):
-    if rank[x] < 0: return x
+    if rank[x] < 0:
+       return x
     rank[x] = root(rank[x])
     return rank[x]
 def unite(x, y):
     x, y = root(x), root(y)
-    if x == y: return False
-    if rank[x] > rank[y]: x, y = y, x
+    if x == y:
+        return False
+    if rank[x] > rank[y]:
+        x, y = y, x
     rank[x] += rank[y]
     rank[y] = x
     return True
-def is_same(x, y): return root(x) == root(y)
-def size(x): return -rank[root(x)]
+def is_same(x, y):
+    return root(x) == root(y)
+def size(x):
+    return -rank[root(x)]
 
-n, m, q = map(int, input().split())
-rank = [-1] * n
-hq = []
-heapify(hq)
+
+n, m, Q = map(int, input().split())
+
+edge = []
 for _ in range(m):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    heappush(hq, (c, a, b, -1))
-for i in range(q):
+    edge.append((c, a, b, -1))
+
+for i in range(Q):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    heappush(hq, (c, a, b, i))
+    edge.append((c, a, b, i))
 
-ans = [0] * q
-while hq:
-    c, a, b, i = heappop(hq)
-    if not is_same(a, b):
-        if i == -1:
-            unite(a, b)
-        else:
-            ans[i] = 1
 
-for a in ans:
-    print('Yes' if a else 'No')
+rank = [-1] * n
+edge.sort()
+ans = ['No'] * Q
+for c, a, b, i in edge:
+    if is_same(a, b):
+        continue
+    if i == -1:
+        unite(a, b)
+    else:
+        ans[i] = 'Yes'
+
+print(*ans)
