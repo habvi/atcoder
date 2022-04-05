76d42b4ad4ec74f6153c064ca4e4c5326bbb8049
habvi <a2231250601h@yahoo.co.jp>
2022-04-05 22:49:41 +0900

    refactor

--- ABC/137/E_CoinsRespawn.py
+++ ABC/137/E_CoinsRespawn.py
@@ -1,58 +1,66 @@
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10 ** 7)
+from collections import defaultdict
+
 def dfs(v):
-    if from1[v]:
+    if front[v]:
         return
-    from1[v] = 1
+    front[v] = 1
     for nv in g[v]:
         dfs(nv)
 
 def rdfs(v):
-    if toN[v]:
+    if tail[v]:
         return
-    toN[v] = 1
+    tail[v] = 1
     for nv in rg[v]:
         rdfs(nv)
 
-from collections import defaultdict
-n, m, p = map(int, input().split())
+def BellmanFord(st):
+    INF = float('inf')
+    dist = [INF] * n
+    dist[st] = 0
+    count_ = 0
+    upd = 1
+    while upd:
+        upd = 0
+        for a, b, c in edges:
+            if not ok[a] or not ok[b]:
+                continue
+            ndst = dist[a] + c
+            if ndst < dist[b]:
+                upd = 1
+                dist[b] = ndst
+        count_ += 1
+        if count_ > n:
+            print(-1)
+            exit()
+    return dist
+
+
+n, m, P = map(int, input().split())
+
 edges = []
 g = defaultdict(list)
 rg = defaultdict(list)
-from1 = [0] * n
-toN = [0] * n
 for _ in range(m):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    edges.append((a, b, -(c - p)))
+    edges.append((a, b, -(c - P)))
     g[a].append(b)
     rg[b].append(a)
 
+front = [0] * n
 dfs(0)
+
+tail = [0] * n
 rdfs(n - 1)
 
 ok = [0] * n
 for i in range(n):
-    ok[i] = from1[i] & toN[i]
+    ok[i] = front[i] & tail[i]
 
-dst = [float('inf')] * n
-dst[0] = 0
-cnt = 0
-upd = 1
-while upd:
-    upd = 0
-    for i in range(m):
-        a, b, c = edges[i]
-        if not ok[a] or not ok[b]:
-            continue
-        ndst = dst[a] + c
-        if ndst < dst[b]:
-            upd = 1
-            dst[b] = ndst
-    cnt += 1
-    if cnt > n:
-        print(-1)
-        exit()
+dist = BellmanFord(0)
 
-ans = -dst[n - 1]
-print(max(0, ans))
+ans = max(0, -dist[n - 1])
+print(ans)
