af3e25a750374fdd0ff84fda4301e0a1576f2eb2
habvi <a2231250601h@yahoo.co.jp>
2022-04-07 21:23:56 +0900

    add: arc013_A

---
+++ ARC/013/A_梱包できるかな？.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+n, m, l = map(int, input().split())
+p, q, r = map(int, input().split())
+
+ans = 0
+for a, b, c in permutations((p, q, r)):
+    ans = max(ans, (n//a) * (m//b) * (l//c))
+
+print(ans)
