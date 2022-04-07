f2e7e93c03bf96185762f8664f1f95b9ec119e19
habvi <a2231250601h@yahoo.co.jp>
2022-04-07 15:45:43 +0900

    refactor

--- ABC/125/D_FlippingSigns.py
+++ ABC/125/D_FlippingSigns.py
@@ -1,12 +1,11 @@
 n = int(input())
 A = list(map(int, input().split()))
 
-minus = sum(a < 0 for a in A)
+total, minus = 0, 0
+mn = float('inf')
+for a in A:
+    total += abs(a)
+    minus += (a < 0)
+    mn = min(mn, abs(a))
 
-B = [abs(a) for a in A]
-total = sum(B)
-
-if minus % 2:
-    print(total - min(B) * 2)
-else:
-    print(total)
+print(total - mn * 2 if minus % 2 else total)
