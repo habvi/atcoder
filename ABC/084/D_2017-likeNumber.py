ae7bc95457e43776b5a2744cbebc93bd9e8221d7
habvi <a2231250601h@yahoo.co.jp>
2022-04-09 20:57:38 +0900

    refactor

--- ABC/084/D_2017-likeNumber.py
+++ ABC/084/D_2017-likeNumber.py
@@ -1,26 +1,30 @@
 from itertools import accumulate
-def furui(x):
-    memo = [0]*(x+1)
-    primes = []
-    for i in range(2, x+1):
-        if memo[i]: continue
-        primes.append(i)
+
+def Sieve(x):
+    memo = [0] * (x + 1)
+    for i in range(2, x + 1):
+        if memo[i]:
+            continue
         memo[i] = i
-        for j in range(i*i, x+1, i):
-            if memo[j]: continue
+        for j in range(i * i, x + 1, i):
+            if memo[j]:
+                continue
             memo[j] = i
-    return memo, primes
+    return memo
 
-MAX = 10**5+5
-memo, primes = furui(MAX)
-simi = [0]*MAX
-for i in range(3, MAX-2, 2):
-    if memo[i] == i and memo[(i+1)//2] == (i+1)//2:
-        simi[i] = 1
 
-ac = list(accumulate(simi))
+mx = 10 ** 5 + 1
+memo = Sieve(mx)
+
+num = [0] * mx
+for i in range(1, mx, 2):
+    x = (i + 1) // 2
+    if memo[i] == i and memo[x] == x:
+        num[i] = 1
+
+ac = list(accumulate(num))
 
 Q = int(input())
 for _ in range(Q):
     l, r = map(int, input().split())
-    print(ac[r] - ac[l-1])
+    print(ac[r] - ac[l - 1])
