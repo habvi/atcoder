357f397009f74860523b02419783b725597ee632
habvi <a2231250601h@yahoo.co.jp>
2022-04-10 16:43:58 +0900

    refactor

--- ABC/243/E_EdgeDeletion.py
+++ ABC/243/E_EdgeDeletion.py
@@ -1,35 +1,28 @@
-from collections import defaultdict
-
 n, m = map(int, input().split())
 
-edges = defaultdict(int)
 INF = float('inf')
-dst = [[INF] * n for _ in range(n)]
+dist = [[INF] * n for _ in range(n)]
 
+edge = []
 for _ in range(m):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    edges[(a, b)] = c
-    dst[a][b] = c
-    dst[b][a] = c
+    dist[a][b] = c
+    dist[b][a] = c
+    edge.append((a, b, c))
 
 for i in range(n):
-    dst[i][i] = 0
+    dist[i][i] = 0
 
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            dst[i][j] = min(dst[i][j], dst[i][k] + dst[k][j])
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 ans = 0
-for i in range(n):
-    for j in range(i + 1, n):
-        cost = edges[(i, j)]
-        if not cost:
-            continue
-
-        for k in range(n):
-            if k not in (i, j) and dst[i][k] + dst[k][j] <= cost:
-                ans += 1
-                break
+for a, b, cost in edge:
+    for k in range(n):
+        if k not in (a, b) and dist[a][k] + dist[k][b] <= cost:
+            ans += 1
+            break
 print(ans)
