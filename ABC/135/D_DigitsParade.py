dcd5bc8e392523ff94543bcfa8459d893e499bea
habvi <a2231250601h@yahoo.co.jp>
2022-04-12 22:20:51 +0900

    refactor

--- ABC/135/D_DigitsParade.py
+++ ABC/135/D_DigitsParade.py
@@ -1,23 +1,28 @@
+def update(now):
+    for pre, num in enumerate(dp):
+        nxt = (now * k + pre) % m
+        ndp[nxt] += num
+        ndp[nxt] %= MOD
+
+
 S = input()
 MOD = 10**9 + 7
-dp = [0] * 13
+
+m = 13
+dp = [0] * m
 dp[0] = 1
-b = 1
+k = 1
 for s in S[::-1]:
-    ndp = [0] * 13
+    ndp = [0] * m
     if s == '?':
-        for k in range(10):
-            for i in range(13):
-                idx = (k*b + i) % 13
-                ndp[idx] += dp[i]
-                ndp[idx] %= MOD
+        for now in range(10):
+            update(now)
     else:
-        s = int(s)
-        for i in range(13):
-            idx = (s*b + i) % 13
-            ndp[idx] += dp[i]
-            ndp[idx] %= MOD
-    b *= 10
-    b %= 13
+        now = int(s)
+        update(now)
+
+    k *= 10
+    k %= m
     dp = ndp
+
 print(dp[5])
