ff8f2317140fc4c253c7a919cd9282cbee6be0ac
habvi <a2231250601h@yahoo.co.jp>
2022-04-13 23:12:03 +0900

    add: bit

--- ABC/187/C_1-SAT.py
+++ ABC/187/C_1-SAT.py
@@ -1,8 +1,28 @@
 n = int(input())
 S = set([input() for _ in range(n)])
-        
+
 for s in S:
     if ''.join(('!', s)) in S:
         print(s)
         exit()
 print('satisfiable')
+
+
+
+# from collections import defaultdict
+
+# n = int(input())
+# S = set(input() for _ in range(n))
+
+# d = defaultdict(int)
+# for s in S:
+#     if s[0] == '!':
+#         d[s[1:]] |= 2
+#     else:
+#         d[s] |= 1
+
+# for k, v in d.items():
+#     if v == 3:
+#         print(k)
+#         exit()
+# print('satisfiable')
