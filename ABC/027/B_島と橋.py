6379562cd8e7ffbd9174986bf58a788d24c8b4cd
habvi <a2231250601h@yahoo.co.jp>
2022-04-13 16:30:45 +0900

    refactor

--- ABC/027/B_島と橋.py
+++ ABC/027/B_島と橋.py
@@ -1,19 +1,18 @@
 n = int(input())
-a = list(map(int, input().split()))
-sm = sum(a)
-k = sm//n
-if sm == 0:
-    print(0)
-    exit()
-if not (sm >= n and sm%n == 0):
+A = list(map(int, input().split()))
+
+if (total := sum(A)) % n:
     print(-1)
     exit()
 
-# iの時点での定員外の人数ch。ch==0のとき橋は不要。
+each = total // n
+
+now, num = 0, 0
 ans = 0
-ch = 0
-for i in a:
-    ch += (i - k)
-    if ch:
-        ans += 1
+for a in A:
+    now += a
+    num += 1
+    if now == each * num:
+        ans += (num - 1)
+        now, num = 0, 0
 print(ans)
