1225ced851f6a55bdfeac50263fa29aa6a6ea3ce
habvi <a2231250601h@yahoo.co.jp>
2022-04-13 23:52:43 +0900

    slight refactor

--- ABC/244/E_KingBombee.py
+++ ABC/244/E_KingBombee.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 n, m, K, S, T, X = map(int, input().split())
-S, T = S - 1, T - 1
+S, T, X = S - 1, T - 1, X - 1
 MOD = 998244353
 
 g = defaultdict(list)
@@ -10,24 +10,21 @@ for _ in range(m):
     g[a].append(b)
     g[b].append(a)
 
-
 dp = [[0] * 2 for _ in range(n)]
 dp[S][0] = 1
 
 for _ in range(K):
     ndp = [[0] * 2 for _ in range(n)]
-    for i in range(n):
-        for ni in g[i]:
-            if ni + 1 == X:
-                ndp[ni][0] += dp[i][1]
-                ndp[ni][0] %= MOD
-                ndp[ni][1] += dp[i][0]
-                ndp[ni][1] %= MOD
+    for v in range(n):
+        for nv in g[v]:
+            if nv == X:
+                ndp[nv][0] += dp[v][1]
+                ndp[nv][1] += dp[v][0]
             else:
-                ndp[ni][0] += dp[i][0]
-                ndp[ni][0] %= MOD
-                ndp[ni][1] += dp[i][1]
-                ndp[ni][1] %= MOD
+                ndp[nv][0] += dp[v][0]
+                ndp[nv][1] += dp[v][1]
+            ndp[nv][0] %= MOD
+            ndp[nv][1] %= MOD
     dp = ndp
 
 print(dp[T][0])
