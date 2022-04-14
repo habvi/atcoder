01fcfdd3d2ff935bbb20ef615399f173fe6af779
habvi <a2231250601h@yahoo.co.jp>
2022-04-15 00:45:36 +0900

    slight refactor

--- ABC/236/E_Average-and-Median.py
+++ ABC/236/E_Average-and-Median.py
@@ -2,13 +2,12 @@ from itertools import groupby
 
 def is_ok1(x):
     # yet, selected
-    dp = [INF] * 2
+    dp = [-INF] * 2
     dp[1] = 0
     for a in A:
-        ndp = [INF] * 2
+        ndp = [-INF] * 2
         ndp[0] = dp[1]
-        ndp[1] = max(ndp[1], dp[0] + a - x)
-        ndp[1] = max(ndp[1], dp[1] + a - x)
+        ndp[1] = max(ndp[1], dp[0] + a - x, dp[1] + a - x)
         dp = ndp
     return max(dp) >= 0
 
@@ -28,7 +27,7 @@ def is_ok2(x):
     for k, v in groupby(B):
         if k == 0:
             zero += len(list(v)) // 2
-    return zero <= one - 1
+    return zero < one
 
 def bisect2(ng, ok):
     while abs(ok - ng) > 1:
@@ -44,8 +43,8 @@ n = int(input())
 A = list(map(int, input().split()))
 
 mx = max(A)
-INF = -float('inf')
+INF = float('inf')
 
 avrg = bisect1(mx + 1, 0)
-med = bisect2(mx + 1, 0)
-print(avrg, med)
+medi = bisect2(mx + 1, 0)
+print(avrg, medi)
