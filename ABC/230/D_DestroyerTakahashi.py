89d76b6d4b5d000a6bb23f4464fb71018209d1a9
habvi <a2231250601h@yahoo.co.jp>
2022-04-16 00:47:02 +0900

    refactor

--- ABC/230/D_DestroyerTakahashi.py
+++ ABC/230/D_DestroyerTakahashi.py
@@ -1,16 +1,13 @@
-from collections import deque
 n, D = map(int, input().split())
-a = [tuple(map(int, input().split())) for _ in range(n)]
-a.sort(key=lambda x : x[1])
-d = deque(a)
+lr = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(n)]
 
-_, r = d.popleft()
-ans = 1
-while d:
-    nl, nr = d.popleft()
-    if nl <= r + D - 1:
-        pass
-    else:
-        r = nr
-        ans += 1
+lr.sort(key=lambda x: x[1])
+
+ans = 0
+right = -1
+for l, r in lr:
+    if l <= right:
+        continue
+    ans += 1
+    right = r + D - 1
 print(ans)
