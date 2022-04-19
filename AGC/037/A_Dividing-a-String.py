47d27287727ded990d8d9007c3c9ef35f5de467d
habvi <a2231250601h@yahoo.co.jp>
2022-04-19 17:15:30 +0900

    refactor

--- AGC/037/A_Dividing-a-String.py
+++ AGC/037/A_Dividing-a-String.py
@@ -1,17 +1,12 @@
-s = input()
-n = len(s)
-cnt = 0
-t = s[0]
-i = 1
-while i < n:
-    if t == s[i]:
-        if i == n - 1:
-            break
-        t = s[i:i+2]
-        cnt += 1
-        i += 2
-    else:
-        t = s[i]
-        cnt += 1
-        i += 1
-print(cnt + 1)
+S = input()
+
+pre = ''
+now = ''
+ans = 0
+for s in S:
+    now += s
+    if pre != now:
+        ans += 1
+        pre = now
+        now = ''
+print(ans)
