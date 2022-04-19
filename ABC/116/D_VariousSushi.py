eeeae2ddb6686cd7a4afaa08bbdc7c3b214bdc68
habvi <a2231250601h@yahoo.co.jp>
2022-04-20 01:09:25 +0900

    refactor

--- ABC/116/D_VariousSushi.py
+++ ABC/116/D_VariousSushi.py
@@ -1,32 +1,31 @@
-from heapq import heappop, heappush
-
 n, K = map(int, input().split())
 td = [tuple(map(int, input().split())) for _ in range(n)]
 
-td.sort(key=lambda x: -x[1])
+td.sort(key=lambda x: x[1])
 
-hq = []
 kind = set()
 total = 0
+ds = []
 for i in range(K):
-    t, d = td[i]
+    t, d = td.pop()
     total += d
     if t not in kind:
         kind.add(t)
     else:
-        heappush(hq, d)
+        ds.append(d)
 
 total += len(kind) ** 2
+ds.sort(reverse=True)
 
 ans = total
-for t, d in td[K:]:
+while ds and td:
+    t, d = td.pop()
     if t in kind:
         continue
     kind.add(t)
 
-    if not hq:
-        break
-    total -= heappop(hq)
-    total += d - (len(kind) - 1) ** 2 + len(kind) ** 2
-    ans = max(ans, total)
+    if ds:
+        total -= ds.pop()
+        total += d - (len(kind) - 1) ** 2 + len(kind) ** 2
+        ans = max(ans, total)
 print(ans)
