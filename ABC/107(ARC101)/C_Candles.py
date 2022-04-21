ce6e598ba29f3ac51142ecce7b64a197fc8bce69
habvi <a2231250601h@yahoo.co.jp>
2022-04-22 01:17:29 +0900

    refactor

--- ABC/107(ARC101)/C_Candles.py
+++ ABC/107(ARC101)/C_Candles.py
@@ -1,17 +1,15 @@
-n, k = map(int, input().split())
-X = list(map(int, input().split()))
-l, r = [], [0]
-for x in X:
-    if x < 0:
-        l.append(-x)
-    else:
-        r.append(x)
-l.append(0)
-l = l[::-1]
+n, K = map(int, input().split())
+A = list(map(int, input().split()))
 
 ans = float('inf')
-for i in range(len(l)):
-    j = k - i
-    if 0 <= j < len(r):
-        ans = min(ans, l[i]*2 + r[j], l[i] + r[j]*2)
+for i in range(n - K + 1):
+    l = A[i]
+    r = A[i + K - 1]
+
+    mn = min(abs(l), abs(r))
+    mx = max(abs(l), abs(r))
+    if l * r >= 0:
+        ans = min(ans, mx)
+    else:
+        ans = min(ans, mn * 2 + mx)
 print(ans)
