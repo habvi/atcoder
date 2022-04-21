100bffca8b4b55a890f28877415b6462176360d8
habvi <a2231250601h@yahoo.co.jp>
2022-04-22 01:57:49 +0900

    refactor

--- ARC/030/B_ツリーグラフ.py
+++ ARC/030/B_ツリーグラフ.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 import sys
 sys.setrecursionlimit(10 ** 7)
+
 def dfs1(v, p):
     for nv in g[v]:
         if nv == p:
@@ -10,17 +12,19 @@ def dfs1(v, p):
         dm[v] |= dm[nv]
 
 def dfs2(v, p):
+    global ans
     for nv in g[v]:
         if nv == p:
             continue
         if not dm[nv]:
             continue
-        ans[0] += 1
+        ans += 1
         dfs2(nv, v)
-        ans[0] += 1
+        ans += 1
 
-from collections import defaultdict
+
 n, x = map(int, input().split())
+x -= 1
 h = list(map(int, input().split()))
 g = defaultdict(list)
 for _ in range(n - 1):
@@ -29,9 +33,9 @@ for _ in range(n - 1):
     g[b].append(a)
 
 dm = [0] * n
-h[x - 1] = 1
-dfs1(x - 1, -1)
+h[x] = 1
+dfs1(x, -1)
 
-ans = [0]
-dfs2(x - 1, -1)
-print(ans[0])
+ans = 0
+dfs2(x, -1)
+print(ans)
--- ARC/030/B_ツリーグラフ_2.py
+++ ARC/030/B_ツリーグラフ_2.py
@@ -1,23 +1,27 @@
-from collections import defaultdict
 import sys
 sys.setrecursionlimit(10 ** 7)
+from collections import defaultdict
+
 def dfs(v, p):
     for nv in g[v]:
         if nv == p:
             continue
         dfs(nv, v)
-        if h[nv] or cnt[nv] > 0:
-            cnt[v] += cnt[nv] + 2
+        if H[nv] or num[nv]:
+            num[v] += num[nv] + 2
 
-n, x = map(int, input().split())
-x -= 1
-h = list(map(int, input().split()))
+
+n, X = map(int, input().split())
+X -= 1
+H = list(map(int, input().split()))
+
 g = defaultdict(list)
 for _ in range(n - 1):
     a, b = map(lambda x: int(x) - 1, input().split())
     g[a].append(b)
     g[b].append(a)
 
-cnt = [0] * n
-dfs(x, -1)
-print(cnt[x])
+num = [0] * n
+dfs(X, -1)
+
+print(num[X])
