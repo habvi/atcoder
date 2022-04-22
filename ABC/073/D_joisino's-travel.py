6f9cc1da4e16ddb83273585e7d893f5c208649b4
habvi <a2231250601h@yahoo.co.jp>
2022-04-22 23:13:57 +0900

    slight refactor

--- ABC/073/D_joisino's-travel.py
+++ ABC/073/D_joisino's-travel.py
@@ -4,27 +4,27 @@ n, m, r = map(int, input().split())
 R = list(map(lambda x: int(x) - 1, input().split()))
 
 INF = float('inf')
-dst = [[INF] * n for _ in range(n)]
+dist = [[INF] * n for _ in range(n)]
 
 for i in range(n):
-    dst[i][i] = 0
+    dist[i][i] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
     a, b = a - 1, b - 1
-    dst[a][b] = min(dst[a][b], c)
-    dst[b][a] = min(dst[b][a], c)
+    dist[a][b] = c
+    dist[b][a] = c
 
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            dst[i][j] = min(dst[i][j], dst[i][k] + dst[k][j])
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 
 def get_dist(R):
     total = 0
     for i in range(1, r):
-        total += dst[R[i - 1]][R[i]]
+        total += dist[R[i - 1]][R[i]]
     return total
 
 
