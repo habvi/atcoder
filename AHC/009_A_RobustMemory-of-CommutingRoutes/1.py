9e6de6d205da99701af994dc85152bdc9f861a21
habvi <a2231250601h@yahoo.co.jp>
2022-04-25 01:40:35 +0900

    move AHC dir later

--- AHC/009_A_RobustMemory-of-CommutingRoutes/1.py
+++
@@ -1,8 +0,0 @@
-sy, sx, gy, gx, P = input().split()
-sy, sx, gy, gx = map(int, [sy, sx, gy, gx])
-P = float(P)
-
-H = [input() for _ in range(20)]
-V = [input() for _ in range(19)]
-
-print('RD' * 100)
