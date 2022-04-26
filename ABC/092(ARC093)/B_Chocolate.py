a0ddfb1f4ccabd695f445d1139d8a5570b6197cc
habvi <a2231250601h@yahoo.co.jp>
2022-04-26 23:24:38 +0900

    refactor

--- ABC/092(ARC093)/B_Chocolate.py
+++ ABC/092(ARC093)/B_Chocolate.py
@@ -1,25 +1,21 @@
 n = int(input())
 D, X = map(int, input().split())
 
-num = [0] * (D + 1)
+ans = n + X
 for _ in range(n):
     a = int(input())
-    for i in range(1, D + 1, a):
-        num[i] += 1
+    ans += (D - 1) // a
+print(ans)
 
-print(sum(num) + X)
-
-
-
-# def ceil(a, b):
-#     return (a + b - 1) // b
 
 
 # n = int(input())
 # D, X = map(int, input().split())
 
-# ans = 0
+# num = [0] * (D + 1)
 # for _ in range(n):
 #     a = int(input())
-#     ans += ceil(D, a)
-# print(ans + X)
+#     for i in range(1, D + 1, a):
+#         num[i] += 1
+
+# print(sum(num) + X)
