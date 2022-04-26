6666b82c2cf6d9ce0c660f682db64a747121d368
habvi <a2231250601h@yahoo.co.jp>
2022-04-26 14:23:50 +0900

    refactor

--- ABC/215/E_ChainContestant.py
+++ ABC/215/E_ChainContestant.py
@@ -7,28 +7,24 @@ S = input()
 MOD = 998244353
 m = 10
 
-dp = [[0] * (1 << m) for _ in range(m)]
+ndp = [[0] * m for _ in range(1 << m)]
 for s in S:
     si = stoi(s)
-    ndp = [[0] * (1 << m) for _ in range(m)]
-    ndp[si][1 << si] += 1
+    dp = [l[:] for l in ndp]
+    ndp[1 << si][si] += 1
 
     for pre in range(1 << m):
         for last in range(m):
             if si == last:
-                ndp[last][pre] += dp[last][pre] * 2
-                ndp[last][pre] %= MOD
+                ndp[pre][last] += dp[pre][last]
+                ndp[pre][last] %= MOD
             else:
-                ndp[last][pre] += dp[last][pre]
-                ndp[last][pre] %= MOD
-
                 if not pre >> si & 1:
-                    ndp[si][pre | 1 << si] += dp[last][pre]
-                    ndp[si][pre | 1 << si] %= MOD
-    dp = [l[:] for l in ndp]
+                    ndp[pre | 1 << si][si] += dp[pre][last]
+                    ndp[pre | 1 << si][si] %= MOD
 
 ans = 0
-for l in dp:
+for l in ndp:
     ans += sum(l)
     ans %= MOD
 print(ans)
