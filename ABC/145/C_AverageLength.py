8119ee67054f320bb1512a454cb5219ed2ea0147
habvi <a2231250601h@yahoo.co.jp>
2022-04-27 22:28:01 +0900

    refactor

--- ABC/145/C_AverageLength.py
+++ ABC/145/C_AverageLength.py
@@ -1,21 +1,18 @@
 from itertools import permutations
 
-def dist(x1, y1, x2, y2):
-    return ((x1 - x2)**2 + (y1 - y2)**2) ** 0.5
+def dist(x, y, x2, y2):
+    return ((x - x2)**2 + (y - y2)**2) ** 0.5
 
 
 n = int(input())
 xy = [tuple(map(int, input().split())) for _ in range(n)]
 
-ans = 0
+total = 0
 num = 0
-for per in permutations(range(n)):
-    total = 0
-    for i in range(n - 1):
-        x1, y1 = xy[per[i]]
-        x2, y2 = xy[per[i + 1]]
-        total += dist(x1, y1, x2, y2)
-    ans += total
+for p in permutations(xy):
     num += 1
+    for i in range(n - 1):
+        (x1, y1), (x2, y2) = p[i], p[i + 1]
+        total += dist(x1, y1, x2, y2)
 
-print(ans / num)
+print(total / num)
