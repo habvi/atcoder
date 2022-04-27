6221abff2eeaf1e73eb0a6d7d7d2f252a50965e2
habvi <a2231250601h@yahoo.co.jp>
2022-04-27 22:30:37 +0900

    slight change

--- ABC/148/E_DoubleFactorial.py
+++ ABC/148/E_DoubleFactorial.py
@@ -1,11 +1,13 @@
 n = int(input())
+
 if n % 2:
     print(0)
     exit()
 
-x = 10
+k = 10
 ans = 0
-while n // x > 0:
-    ans += n // x
-    x *= 5
+while k <= n:
+    ans += n // k
+    k *= 5
+
 print(ans)
