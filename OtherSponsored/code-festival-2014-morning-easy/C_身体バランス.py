a1975b846778b61686a116a5fda83f1130aa186a
habvi <a2231250601h@yahoo.co.jp>
2022-04-28 17:01:51 +0900

    refactor

--- OtherSponsored/code-festival-2014-morning-easy/C_身体バランス.py
+++ OtherSponsored/code-festival-2014-morning-easy/C_身体バランス.py
@@ -1,36 +1,39 @@
-import heapq
+from heapq import heappop, heappush
+from collections import defaultdict
+
 def dijkstra(st):
-    dst = [float('inf')] * n
-    dst[st] = 0
+    dist = [INF] * n
+    dist[st] = 0
     hq = [(0, st)]
     while hq:
-        c, v = heapq.heappop(hq)
-        if c > dst[v]: continue
-        for nc, nv in g[v]:
-            if c + nc >= dst[nv]: continue
-            dst[nv] = c + nc
-            heapq.heappush(hq, (c + nc, nv))
-    return dst
+        c, v = heappop(hq)
+        if c > dist[v]:
+            continue
 
-from collections import defaultdict
+        for nc, nv in g[v]:
+            if c + nc >= dist[nv]:
+                continue
+            dist[nv] = c + nc
+            heappush(hq, (c + nc, nv))
+    return dist
+
+
 n, m = map(int, input().split())
-s, t = map(lambda x: int(x) - 1, input().split())
+S, T = map(lambda x: int(x) - 1, input().split())
+
 g = defaultdict(list)
 for _ in range(m):
-    x, y, d = map(int, input().split())
+    x, y, c = map(int, input().split())
     x, y = x - 1, y - 1
-    g[x].append((d, y))
-    g[y].append((d, x))
+    g[x].append((c, y))
+    g[y].append((c, x))
 
-ds = dijkstra(s)
-dt = dijkstra(t)
-for i in range(n):
-    if i in (s, t):
-        continue
-    d1, d2 = ds[i], dt[i]
-    if float('inf') in (d1, d2):
-        continue
-    if d1 == d2:
-        print(i + 1)
+INF = float('inf')
+from_s = dijkstra(S)
+from_t = dijkstra(T)
+
+for v, (d, d2) in enumerate(zip(from_s, from_t)):
+    if d != INF and d == d2:
+        print(v + 1)
         exit()
 print(-1)
