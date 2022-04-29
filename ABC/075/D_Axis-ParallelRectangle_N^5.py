2b247fbd7a77b080eec354eec8f96c30ab614be0
habvi <a2231250601h@yahoo.co.jp>
2022-04-29 23:57:26 +0900

    slight refactor

--- ABC/075/D_Axis-ParallelRectangle_N^5.py
+++ ABC/075/D_Axis-ParallelRectangle_N^5.py
@@ -1,22 +1,23 @@
-n, k = map(int, input().split())
+n, K = map(int, input().split())
 xy = [tuple(map(int, input().split())) for _ in range(n)]
-x_st = sorted(xy, key=lambda x: x[0])
-y_st = sorted(xy, key=lambda x: x[1])
+
+X = sorted(xy, key=lambda x: x[0])
+Y = sorted(xy, key=lambda x: x[1])
 
 ans = float('inf')
 for i in range(n):
-    for j in range(n):
-        lx = x_st[i][0]
-        ly = y_st[j][1]
-        for ii in range(i+1, n):
-            for jj in range(j+1, n):
-                rx = x_st[ii][0]
-                ry = y_st[jj][1]
+    for j in range(i + 1, n):
+        lx, rx = X[i][0], X[j][0]
 
-                cnt = 0
+        for k in range(n):
+            for l in range(k + 1, n):
+                ly, ry = Y[k][1], Y[l][1]
+
+                total = 0
                 for x, y in xy:
                     if lx <= x <= rx and ly <= y <= ry:
-                        cnt += 1
-                if cnt >= k:
-                    ans = min(ans, (rx - lx) * (ry - ly))
+                        total += 1
+
+                if total >= K:
+                    ans = min(ans, (rx - lx)*(ry - ly))
 print(ans)
