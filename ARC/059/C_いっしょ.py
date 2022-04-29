2e7727b91c5dc8b4a753f3e56f30623945789d72
habvi <a2231250601h@yahoo.co.jp>
2022-04-30 02:06:48 +0900

    del

--- ARC/059/C_いっしょ.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-ans = float('inf')
-for x in range(min(a), max(a) + 1):
-    c = 0
-    for i in range(n):
-        c += (a[i] - x) ** 2
-    ans = min(ans, c)
-print(ans)
