ed95976e1aecb8fef366c55800c6dd0ebc5d3003
habvi <a2231250601h@yahoo.co.jp>
2022-05-03 01:33:29 +0900

    refactor

--- ABC/104/C_AllGreen_DP.py
+++ ABC/104/C_AllGreen_DP.py
@@ -1,21 +1,19 @@
 D, G = map(int, input().split())
 pc = [tuple(map(int, input().split())) for _ in range(D)]
 
-n = 1001
-dp = [0] * n
+mx = 100 * 10
+dp = [0] * (mx + 1)
 for i, (q, bonus) in enumerate(pc, 1):
-    ndp = [0] * n
-
-    for j in range(n):
+    ndp = [0] * (mx + 1)
+    for j in range(mx + 1):
         for num in range(q):
-            if j + num < n:
-                ndp[j + num] = max(ndp[j + num], dp[j] + i * 100 * num)
-        if j + q < n:
-            ndp[j + q] = max(ndp[j + q], dp[j] + i * 100 * q + bonus)
-
+            if j + num <= mx:
+                ndp[j + num] = max(ndp[j + num], dp[j] + 100*i*num)
+        if j + q <= mx:
+            ndp[j + q] = max(ndp[j + q], dp[j] + 100*i*q + bonus)
     dp = ndp
 
-for i, point in enumerate(dp):
-    if point >= G:
+for i, score in enumerate(dp):
+    if score >= G:
         print(i)
         exit()
