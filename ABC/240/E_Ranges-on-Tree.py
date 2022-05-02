5242232861afe33d024dba06ef68d8103f982ec1
habvi <a2231250601h@yahoo.co.jp>
2022-05-02 14:58:42 +0900

    refactor

--- ABC/240/E_Ranges-on-Tree.py
+++ ABC/240/E_Ranges-on-Tree.py
@@ -3,21 +3,22 @@ sys.setrecursionlimit(10 ** 7)
 from collections import defaultdict
 
 def dfs(v, p):
+    global num
     for nv in g[v]:
         if nv == p:
             continue
         dfs(nv, v)
 
         if L[nv] == INF:
-            L[nv] = inc[0]
-            R[nv] = inc[0]
-            inc[0] += 1
+            L[nv] = num
+            R[nv] = num
+            num += 1
+
         L[v] = min(L[v], L[nv])
         R[v] = max(R[v], R[nv])
 
 
 n = int(input())
-
 g = defaultdict(list)
 for _ in range(n - 1):
     a, b = map(lambda x: int(x) - 1, input().split())
@@ -26,9 +27,9 @@ for _ in range(n - 1):
 
 INF = float('inf')
 L = [INF] * n
-R = [-INF] * n
-inc = [1]
+R = [0] * n
 
+num = 1
 dfs(0, -1)
 
 for l, r in zip(L, R):
