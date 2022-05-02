5722564b2f86a02115689889415a7f5224589059
habvi <a2231250601h@yahoo.co.jp>
2022-05-03 00:42:40 +0900

    refactor

--- ABC/216/D_Pair-of-Balls.py
+++ ABC/216/D_Pair-of-Balls.py
@@ -1,30 +1,28 @@
-from collections import deque, defaultdict
-n, m = map(int, input().split())
-q = deque([])
-cnt = defaultdict(int)
-A = []
-for i in range(m):
-    k = int(input())
-    a = list(map(lambda x: int(x) - 1, input().split()))
-    ba = a.pop()
-    if ba in cnt:
-        q.append(cnt[ba])
-        q.append(i)
-    else:
-        cnt[ba] = i
-    A.append(a)
+from collections import defaultdict
 
-tot = 0
-while q:
-    x = q.popleft()
-    y = q.popleft()
-    for i in (x, y):
-        if A[i]:
-            ba = A[i].pop()
-            if ba in cnt:
-                q.append(cnt[ba])
-                q.append(i)
-            else:
-                cnt[ba] = i
-    tot += 1
-print('Yes' if tot == n else 'No')
+n, m = map(int, input().split())
+
+balls = []
+for _ in range(m):
+    k = int(input())
+    A = list(map(int, input().split()))
+    balls.append(A[::-1])
+
+selected = defaultdict(lambda : -1)
+
+check = list(range(m))
+while check:
+    i = check.pop()
+    ball = balls[i].pop()
+
+    if selected[ball] == -1:
+        selected[ball] = i
+    else:
+        if balls[i]:
+            check.append(i)
+        j = selected[ball]
+        if balls[j]:
+            check.append(j)
+        del selected[ball]
+
+print('Yes' if not selected else 'No')
