70b1b7483b01571edd11337c24edca8599d7ad63
habvi <a2231250601h@yahoo.co.jp>
2022-05-05 01:41:58 +0900

    add: 212_C

---
+++ ABC/212/C_MinDifference.py
@@ -0,0 +1,16 @@
+from bisect import bisect
+
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+INF = float('inf')
+B.append(-INF)
+B.append(INF)
+B.sort()
+
+ans = INF
+for a in A:
+    bi = bisect(B, a)
+    ans = min(ans, B[bi] - a, a - B[bi - 1])
+print(ans)
