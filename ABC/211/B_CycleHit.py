4918b5310297d2268128bfed286d4331e85959a5
habvi <a2231250601h@yahoo.co.jp>
2022-05-05 01:06:46 +0900

    refactor

--- ABC/211/B_CycleHit.py
+++ ABC/211/B_CycleHit.py
@@ -1,3 +1,3 @@
-t = ['H', '2B', '3B', 'HR']
-s = [input() for _ in range(4)]
-print('Yes' if sorted(t) == sorted(s) else 'No')
+S = set(input() for _ in range(4))
+
+print('Yes' if len(S) == 4 else 'No')
