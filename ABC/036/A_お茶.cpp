16603e54487a2d6e71c53d4159290ef09a86e163
habvi <a2231250601h@yahoo.co.jp>
2022-05-11 18:07:11 +0900

    add: 036_A.cpp

---
+++ ABC/036/A_お茶.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+
+int main(void) {
+    int A, B;
+    cin >> A >> B;
+
+    cout << (B + A - 1) / A << endl;
+    return 0;
+}
