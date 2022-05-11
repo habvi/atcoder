72220cd4232eb8817128a6916537c49901bff461
habvi <a2231250601h@yahoo.co.jp>
2022-05-11 17:57:05 +0900

    add: 125_A.cpp

---
+++ ABC/125/A_BiscuitGenerator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+
+int main(void) {
+    int A, B, T;
+    cin >> A >> B >> T;
+
+    cout << T / A * B << endl;
+    return 0;
+}
