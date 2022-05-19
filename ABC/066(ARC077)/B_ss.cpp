0dc1156a590577b42b8b21e7923cc06b35547190
habvi <a2231250601h@yahoo.co.jp>
2022-05-20 01:19:15 +0900

    add: another way

--- ABC/066(ARC077)/B_ss.cpp
+++ ABC/066(ARC077)/B_ss.cpp
@@ -19,3 +19,31 @@ int main(void) {
         }
     }
 }
+
+
+
+// ----------
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main(void) {
+    string s;
+    cin >> s;
+ 
+    s.pop_back();
+    s.pop_back();
+    int n = s.size();
+    while (n >= 2) {
+        int mid = n / 2;
+        string l = s.substr(0, mid), r = s.substr(mid, mid);
+        if (l == r) {
+            cout << n << endl;
+            return 0;
+        }
+        s.pop_back();
+        s.pop_back();
+        n -= 2;
+    }
+}
