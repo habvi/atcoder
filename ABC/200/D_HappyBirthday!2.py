bfcc297948567405952bea5be47779e8d9ee900a
habvi <a2231250601h@yahoo.co.jp>
2022-07-25 23:07:46 +0900

    functionalize

--- ABC/200/D_HappyBirthday!2.py
+++ ABC/200/D_HappyBirthday!2.py
@@ -1,36 +1,29 @@
-n = int(input())
+def calc(bit):
+    total = 0
+    lis = []
+    for i in range(N):
+        if bit >> i & 1:
+            total += A[i]
+            lis.append(i + 1)
+    return total % MOD, lis
+
+
+N = int(input())
 A = list(map(int, input().split()))
+
 MOD = 200
-
 # 2**8 - 1 = 255 > 200
-n = min(n, 8)
-A = [a % MOD for a in A[:n]]
-
-for b in range(1, 1 << n):
-    for c in range(1, 1 << n):
-        if b == c:
+N = min(N, 8)
+for bit1 in range(1, 1 << N):
+    for bit2 in range(1, 1 << N):
+        if bit1 == bit2:
             continue
 
-        B = []
-        sumb = 0
-        for i in range(n):
-            if b >> i & 1:
-                B.append(i + 1)
-                sumb += A[i]
-                sumb %= MOD
-
-        C = []
-        sumc = 0
-        for i in range(n):
-            if c >> i & 1:
-                C.append(i + 1)
-                sumc += A[i]
-                sumc %= MOD
-
-        if sumb == sumc:
-            print('Yes')
+        sum_b, B = calc(bit1)
+        sum_c, C = calc(bit2)
+        if sum_b == sum_c:
+            print("Yes")
             print(len(B), *B)
             print(len(C), *C)
             exit()
-
-print('No')
+print("No")
