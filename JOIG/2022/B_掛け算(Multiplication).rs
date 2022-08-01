05e31e2d25daf3dd50a26abf98d8936474e97e9a
habvi <a2231250601h@yahoo.co.jp>
2022-08-01 21:28:34 +0900

    add: joig2022_B

---
+++ JOIG/2022/B_掛け算(Multiplication).rs
@@ -0,0 +1,32 @@
+use std::io::stdin;
+use std::str::FromStr;
+
+fn input_t<T: FromStr>() -> T {
+    let mut s: String = String::new();
+    stdin().read_line(&mut s).ok().unwrap();
+    s.trim().parse().ok().unwrap()
+}
+
+fn input_vec<T: FromStr>() -> Vec<T> {
+    input_t::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn main() {
+    let n: usize = input_t();
+    let a: Vec<usize> = input_vec();
+
+    let mut ans: usize = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if a[i] * a[j] == a[k] {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
