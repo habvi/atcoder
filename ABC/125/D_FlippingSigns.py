bfd9f5751575d3fd961a081bddf7ac4a5b5cb4e5
habvi <a2231250601h@yahoo.co.jp>
2022-08-03 23:47:34 +0900

    125_D another ver.

--- ABC/125/D_FlippingSigns.py
+++ ABC/125/D_FlippingSigns.py
@@ -9,3 +9,19 @@ for a in A:
     mn = min(mn, abs(a))
 
 print(total - mn * 2 if minus % 2 else total)
+
+
+
+# -------------------------------
+N = int(input())
+A = list(map(int, input().split()))
+
+minus = 0
+for a in A:
+    minus += (a < 0)
+
+B = list(map(abs, A))
+if minus % 2:
+    print(sum(B) - min(B) * 2)
+else:
+    print(sum(B))
