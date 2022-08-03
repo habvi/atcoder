804279b7c7e4cedbb3a453402409a017ab6e28cc
habvi <a2231250601h@yahoo.co.jp>
2022-08-03 23:41:53 +0900

    refactor

--- ABC/151/C_Welcome-to-AtCoder.py
+++ ABC/151/C_Welcome-to-AtCoder.py
@@ -1,20 +1,17 @@
 from collections import defaultdict
-n, m = map(int, input().split())
-ac = set()
-wa = defaultdict(int)
-for _ in range(m):
-    p, s = input().split()
-    p = int(p)
-    if p in ac:
-        continue
-    if s == 'AC':
-        ac.add(p)
-    else:
-        wa[p] += 1
 
-a, w = 0, 0
-for i in range(1, n + 1):
-    if i in ac:
-        a += 1
-        w += wa[i]
-print(a, w)
+N, M = map(int, input().split())
+
+q = defaultdict(int)
+ac, wa = 0, 0
+for _ in range(M):
+    p, S = input().split()
+    p = int(p)
+    if q[p] != -1:
+        if S == "WA":
+            q[p] += 1
+        else:
+            ac += 1
+            wa += q[p]
+            q[p] = -1
+print(ac, wa)
