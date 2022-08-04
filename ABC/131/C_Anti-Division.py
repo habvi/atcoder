939984c6f111d91953aaddb9ce7ba2ce2d4087c4
habvi <a2231250601h@yahoo.co.jp>
2022-08-05 02:26:08 +0900

    add: another function way

--- ABC/131/C_Anti-Division.py
+++ ABC/131/C_Anti-Division.py
@@ -15,3 +15,23 @@ a, b, c, d = map(int, input().split())
 
 lcm_cd = lcm(c, d)
 print(f(b) - f(a - 1))
+
+
+
+# ---------------------------
+from math import gcd
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def calc(x):
+    return B // x - (A - 1) // x
+
+
+A, B, C, D = map(int, input().split())
+
+total = B - A + 1
+c = calc(C)
+d = calc(D)
+cd = calc(lcm(C, D))
+print(total - c - d + cd)
