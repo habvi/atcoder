4f420ad4337f3616d9acc3af1c057a8201d16b14
habvi <a2231250601h@yahoo.co.jp>
2022-08-08 01:12:19 +0900

    add: another way

--- ABC/085/C_Otoshidama.py
+++ ABC/085/C_Otoshidama.py
@@ -9,3 +9,17 @@ for i in range(n + 1):
             print(i, j, k)
             exit()
 print(-1, -1, -1)
+
+
+# ------------------------------
+N, Y = map(int, input().split())
+
+for x in range(N + 1):
+    for y in range(N + 1):
+        z = N - x - y
+        if z < 0:
+            continue
+        if x * 10000 + y * 5000 + z * 1000 == Y:
+            print(x, y, z)
+            exit()
+print(-1, -1, -1)
