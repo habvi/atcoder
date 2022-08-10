d2cba45b1a664278fa4a5cca72fa9a7ea22ce8cf
habvi <a2231250601h@yahoo.co.jp>
2022-08-11 00:00:44 +0900

    update

--- ABC/195/B_ManyOranges.py
+++ ABC/195/B_ManyOranges.py
@@ -1,9 +1,31 @@
-a, b, kg = map(int, input().split())
-g = 1000 * kg
+def ceil(a, b):
+    return (a + b - 1) // b
 
-mn = (g + b - 1) // b
-mx = g // a
+
+A, B, W = map(int, input().split())
+
+W *= 1000
+mn = ceil(W, B)
+mx = W // A
 if mn <= mx:
     print(mn, mx)
 else:
-    print('UNSATISFIABLE')
+    print("UNSATISFIABLE")
+
+
+
+# ------------------------
+a, b, w = map(int, input().split())
+
+w *= 1000
+m = []
+for i in range(w//b - 5, w//a + 5):
+    if i == 0:
+        continue
+    if a <= w / i <= b:
+        m.append(i)
+
+if m:
+    print(min(m), max(m))
+else:
+    print("UNSATISFIABLE")
