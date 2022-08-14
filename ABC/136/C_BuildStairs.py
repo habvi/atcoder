4f6a73fbe4c1a881fc3e59f51ff6040c4241d8b9
habvi <a2231250601h@yahoo.co.jp>
2022-08-15 01:26:29 +0900

    refactor

--- ABC/136/C_BuildStairs.py
+++ ABC/136/C_BuildStairs.py
@@ -1,14 +1,10 @@
-n = int(input())
+N = int(input())
 H = list(map(int, input().split()))
 
-H[0] -= 1
-for i in range(1, n):
-    if H[i] > H[i - 1]:
-        H[i] -= 1
-    elif H[i] == H[i - 1]:
-        continue
-    else:
-        print('No')
+for i in range(N - 1):
+    if H[i] < H[i + 1]:
+        H[i + 1] -= 1
+    if H[i] > H[i + 1]:
+        print("No")
         exit()
-
-print('Yes')
+print("Yes")
