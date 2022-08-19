1678520454733d0090eddecf497b5bd9f9e456ab
habvi <a2231250601h@yahoo.co.jp>
2022-08-20 00:46:43 +0900

    add: another ver.

--- ABC/251/C_PoemOnlineJudge.py
+++ ABC/251/C_PoemOnlineJudge.py
@@ -12,3 +12,22 @@ for i in range(n):
 score = list(idx.values())
 score.sort(key=lambda x: (-x[0], x[1]))
 print(score[0][1])
+
+
+
+# ---------------------------
+N = int(input())
+
+used = set()
+mx = 0
+ans = None
+for i in range(N):
+    s, t = input().split()
+    t = int(t)
+    if s in used:
+        continue
+    used.add(s)
+    if t > mx:
+        ans = i + 1
+        mx = t
+print(ans)
