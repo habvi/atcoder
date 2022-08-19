4acde5c6e0e7db8d19a5da0fb47a4ab6e221764d
habvi <a2231250601h@yahoo.co.jp>
2022-08-20 00:53:24 +0900

    refactor

--- ABC/159/D_BannedK.py
+++ ABC/159/D_BannedK.py
@@ -1,20 +1,17 @@
-from collections import Counter, defaultdict
+from collections import Counter
 
-def nC2(n):
-    return n*(n - 1) // 2
+def comb(x):
+    return x * (x - 1) // 2
 
 
-n = int(input())
+N = int(input())
 A = list(map(int, input().split()))
 
 c = Counter(A)
 total = 0
-case = defaultdict(int)
-case2 = defaultdict(int)
-for k, v in c.items():
-    total += nC2(v)
-    case[k] = nC2(v)
-    case2[k] = nC2(v - 1)
+for v in c.values():
+    if v >= 2:
+        total += comb(v)
 
 for a in A:
-    print(total - case[a] + case2[a])
+    print(total - comb(c[a]) + comb(c[a] - 1))
