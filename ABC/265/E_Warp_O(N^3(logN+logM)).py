d5ad58e6262284bc77ead5d2d75ac29e74e0136a
habvi <a2231250601h@yahoo.co.jp>
2022-08-23 00:34:48 +0900

    faster DP

--- ABC/265/E_Warp_O(N^3(logN+logM)).py
+++ ABC/265/E_Warp_O(N^3(logN+logM)).py
@@ -4,11 +4,10 @@ N, M = map(int, input().split())
 A, B, C, D, E, F = map(int, input().split())
 ng = set()
 for _ in range(M):
-    x, y= map(int, input().split())
+    x, y = map(int, input().split())
     ng.add((x, y))
 MOD = 998244353
 
-INF = float('inf')
 dp = defaultdict(int)
 dp[(0, 0)] = 1
 for _ in range(N):
@@ -17,7 +16,39 @@ for _ in range(N):
         for dx, dy in ((A, B), (C, D), (E, F)):
             nx, ny = x + dx, y + dy
             if (nx, ny) not in ng:
-                ndp[(nx, ny)] += v % MOD
+                ndp[(nx, ny)] += (v % MOD)
+    dp = ndp
+ans = sum(dp.values()) % MOD
+print(ans)
+
+
+
+# --------------------------------------
+# faster
+from collections import defaultdict
+
+N, M = map(int, input().split())
+A, B, C, D, E, F = map(int, input().split())
+ng = set()
+for _ in range(M):
+    x, y = map(int, input().split())
+    ng.add((x, y))
+MOD = 998244353
+
+dp = defaultdict(int)
+dp[(0, 0)] = 1
+for _ in range(N):
+    ndp = defaultdict(int)
+    for (x, y), v in dp.items():
+        nx, ny = x + A, y + B
+        if (nx, ny) not in ng:
+            ndp[(nx, ny)] += (v % MOD)
+        nx, ny = x + C, y + D
+        if (nx, ny) not in ng:
+            ndp[(nx, ny)] += (v % MOD)
+        nx, ny = x + E, y + F
+        if (nx, ny) not in ng:
+            ndp[(nx, ny)] += (v % MOD)
     dp = ndp
 ans = sum(dp.values()) % MOD
 print(ans)
