02cc7f94c1215f0f9352d0d957e64e2b5fc8ae91
habvi <a2231250601h@yahoo.co.jp>
2022-08-24 02:19:40 +0900

    insert -> emplace, pair -> P

--- ABC/265/E_Warp_O(N^3(logN+logM)).cpp
+++ ABC/265/E_Warp_O(N^3(logN+logM)).cpp
@@ -3,52 +3,53 @@ using namespace std;
 #define _GLIBCXX_DEBUG
 #define rep(i, a, b) for (int i = a; i < b; ++i)
 using ll = long long;
+using P = pair<ll, ll>;
 
 int main(void) {
     int N, M;
     cin >> N >> M;
     ll A, B, C, D, E, F;
     cin >> A >> B >> C >> D >> E >> F;
-    set<pair<ll, ll>> s;
+    set<P> s;
     rep(_, 0, M) {
         ll x, y;
         cin >> x >> y;
-        s.insert({x, y});
+        s.emplace(x, y);
     }
     const int MOD = 998244353;
 
-    map<pair<ll, ll>, int> dp;
-    dp[{0, 0}] = 1;
+    map<P, int> dp;
+    dp[P(0, 0)] = 1;
     rep(_, 0, N) {
-        map<pair<ll, ll>, int> ndp;
-        for (auto [k, v] : dp) {
+        map<P, int> ndp;
+        for (auto &[k, v] : dp) {
             ll x = k.first;
             ll y = k.second;
             ll nx = x + A;
             ll ny = y + B;
-            if (!s.count({nx, ny})) {
-                ndp[{nx, ny}] += v;
-                ndp[{nx, ny}] %= MOD;
+            if (!s.count(P(nx, ny))) {
+                ndp[P(nx, ny)] += v;
+                ndp[P(nx, ny)] %= MOD;
             }
             nx = x + C;
             ny = y + D;
-            if (!s.count({nx, ny})) {
-                ndp[{nx, ny}] += v;
-                ndp[{nx, ny}] %= MOD;
+            if (!s.count(P(nx, ny))) {
+                ndp[P(nx, ny)] += v;
+                ndp[P(nx, ny)] %= MOD;
             }
             nx = x + E;
             ny = y + F;
-            if (!s.count({nx, ny})) {
-                ndp[{nx, ny}] += v;
-                ndp[{nx, ny}] %= MOD;
+            if (!s.count(P(nx, ny))) {
+                ndp[P(nx, ny)] += v;
+                ndp[P(nx, ny)] %= MOD;
             }
         }
         swap(dp, ndp);
     }
 
     ll ans = 0;
-    for (auto [k, v] : dp) {
-        ans += v;
+    for (auto &x : dp) {
+        ans += x.second;
         ans %= MOD;
     }
     cout << ans << endl;
