0787f5b2cfa63af3817c01d8e850d4aaeb7eb601
habvi <a2231250601h@yahoo.co.jp>
2022-08-25 02:58:24 +0900

    fixed

--- ABC/166/E_ThisMessageWillSelf-Destruct-in-5s.py
+++ ABC/166/E_ThisMessageWillSelf-Destruct-in-5s.py
@@ -1,11 +1,22 @@
 from collections import defaultdict
-n = int(input())
-A = list(map(int, input().split()))
-d = defaultdict(int)
-for i, a in enumerate(A):
-    d[i - a] += 1
 
-ans = 0
+N = int(input())
+A = list(map(int, input().split()))
+
+X, Y = [], []
 for i, a in enumerate(A):
-    ans += d[i + a]
+    X.append(i + a)
+    Y.append(i - a)
+
+num = defaultdict(int)
+ans = 0
+for x, y in zip(X, Y):
+    ans += num[y]
+    num[x] += 1
 print(ans)
+
+
+# |j - i| = Ai + Aj
+# i < j
+# j - i = Ai + Aj
+# i + Ai = j - Aj
