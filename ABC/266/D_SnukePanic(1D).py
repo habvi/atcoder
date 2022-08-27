0fcd46620550843dd010e528e294ba00451499bb
habvi <a2231250601h@yahoo.co.jp>
2022-08-28 01:13:45 +0900

    refactor

--- ABC/266/D_SnukePanic(1D).py
+++ ABC/266/D_SnukePanic(1D).py
@@ -8,22 +8,18 @@ for _ in range(N):
 INF = float('inf')
 dp = [-INF] * 5
 dp[0] = 0
-for now in range(1, mx + 1):
+for t in range(1, mx + 1):
     ndp = [-INF] * 5
+    exist = 0
+    if t in hole:
+        x, a = hole[t]
+        exist = 1
     for i in range(5):
-        ndp[i] = max(ndp[i], dp[i])
+        plus = a if exist and i == x else 0
+        ndp[i] = max(ndp[i], dp[i] + plus)
         if i - 1 >= 0:
-            ndp[i] = max(ndp[i], dp[i - 1])
+            ndp[i] = max(ndp[i], dp[i - 1] + plus)
         if i + 1 < 5:
-            ndp[i] = max(ndp[i], dp[i + 1])
-    if now not in hole:
-        dp = ndp
-        continue
-    x, a = hole[now]
-    ndp[x] = max(ndp[x], dp[x] + a)
-    if x - 1 >= 0:
-        ndp[x] = max(ndp[x], dp[x - 1] + a)
-    if x + 1 < 5:
-        ndp[x] = max(ndp[x], dp[x + 1] + a)
+            ndp[i] = max(ndp[i], dp[i + 1] + plus)
     dp = ndp
 print(max(dp))
