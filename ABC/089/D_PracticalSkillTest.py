def7cad21af62e1a1f4ebe2fb55da63b0feec949
habvi <a2231250601h@yahoo.co.jp>
2022-08-29 03:34:33 +0900

    refactor

--- ABC/089/D_PracticalSkillTest.py
+++ ABC/089/D_PracticalSkillTest.py
@@ -1,26 +1,21 @@
-def mh(A, B):
-    x1, y1 = A
-    x2, y2 = B
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-h, w, D = map(int, input().split())
-A = [tuple(map(int, input().split())) for _ in range(h)]
+H, W, D = map(int, input().split())
+A = [tuple(map(int, input().split())) for _ in range(H)]
 
 xy = {}
-for i in range(h):
-    for j in range(w):
+for i in range(H):
+    for j in range(W):
         xy[A[i][j]] = (i, j)
 
-hw = h * w
-dist = [0] * (hw + 1)
-for i in range(D + 1, hw + 1):
-    if dist[i]:
-        break
-    for j in range(i, hw + 1, D):
-        dist[j] = dist[j - D] + mh(xy[j], xy[j - D])
+total = H * W
+ac = [0] * (total + 1)
+for num in range(1, D + 1):
+    for i in range(num + D, total + 1, D):
+        px, py = xy[i - D]
+        x, y = xy[i]
+        cost = abs(x - px) + abs(y - py)
+        ac[i] = ac[i - D] + cost
 
 Q = int(input())
 for _ in range(Q):
     l, r = map(int, input().split())
-    print(dist[r] - dist[l])
+    print(ac[r] - ac[l])
