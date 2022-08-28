e326d8644f51cd879e1862556fb3285ebf278b8d
habvi <a2231250601h@yahoo.co.jp>
2022-08-28 23:19:30 +0900

    refactor

--- ABC/190/C_Bowls-and-Dishes.py
+++ ABC/190/C_Bowls-and-Dishes.py
@@ -1,19 +1,18 @@
-n, m = map(int, input().split())
-ab = [tuple(map(int, input().split())) for _ in range(m)]
+N, M = map(int, input().split())
+ab = [tuple(map(int, input().split())) for _ in range(M)]
 K = int(input())
 cd = [tuple(map(int, input().split())) for _ in range(K)]
-ans = 0
-for i in range(1 << K):
-    put = [0] * n
-    for j in range(K):
-        if i & (1<<j):
-            put[cd[j][0] - 1] = 1
-        else:
-            put[cd[j][1] - 1] = 1
 
-    cnt = 0
+ans = 0
+for bit in range(1 << K):
+    ball = set()
+    for i, (c, d) in enumerate(cd):
+        if bit >> i & 1:
+            ball.add(c)
+        else:
+            ball.add(d)
+    total = 0
     for a, b in ab:
-        if put[a - 1] and put[b - 1]:
-            cnt += 1
-    ans = max(ans, cnt)
+        total += (a in ball) and (b in ball)
+    ans = max(ans, total)
 print(ans)
