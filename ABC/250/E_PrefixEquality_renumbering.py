f5b45a8dcb1b4dd9004b241f2998a3c9c4caa419
habvi <a2231250601h@yahoo.co.jp>
2022-08-29 23:34:58 +0900

    update

--- ABC/250/E_PrefixEquality_renumbering.py
+++ ABC/250/E_PrefixEquality_renumbering.py
@@ -1,33 +1,35 @@
 # hamamuさん解
 from collections import defaultdict
 
-n = int(input())
+N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 INF = float('inf')
 num = defaultdict(lambda : INF)
-a_mx = [0] * n
-x = 1
-for i, a in enumerate(A):
+mx_a = []
+new = 0
+for a in A:
     if num[a] == INF:
-        num[a] =  x
-        a_mx[i] = x
-        x += 1
-    else:
-        a_mx[i] = a_mx[i - 1]
+        new += 1
+        num[a] =  new
+    mx_a.append(new)
 
 mx = 0
-b_kind = [None] * n
+mx_b = []
 s = set()
-b_mx = [0] * n
-for i, b in enumerate(B):
-    s.add(num[b])
-    b_kind[i] = len(s)
-    mx = max(mx, num[b])
-    b_mx[i] = mx
+kind_b = []
+for b in B:
+    nb = num[b]
+    mx = max(mx, nb)
+    mx_b.append(mx)
+    s.add(nb)
+    kind_b.append(len(s))
 
 Q = int(input())
 for _ in range(Q):
     x, y = map(lambda x: int(x) - 1, input().split())
-    print("Yes" if a_mx[x] == b_kind[y] == b_mx[y] else "No")
+    if mx_a[x] == mx_b[y] == kind_b[y]:
+        print("Yes")
+    else:
+        print("No")
