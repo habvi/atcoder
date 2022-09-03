e7293f1a787c804660f33f5e1c26ac66a8d9fe66
habvi <a2231250601h@yahoo.co.jp>
2022-09-03 16:50:41 +0900

    add: another ver.

--- ABC/220/B_BaseK.py
+++ ABC/220/B_BaseK.py
@@ -13,3 +13,11 @@ B = list(map(int, list(B)))
 a = n_to_deci(A, K)
 b = n_to_deci(B, K)
 print(a * b)
+
+
+
+# -------------------------
+K = int(input())
+A, B = input().split()
+
+print(int(A, K) * int(B, K))
