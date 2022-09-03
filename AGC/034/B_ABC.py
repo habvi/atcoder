a322597eafeeb480e20711fbc93ef7bb9c51bccc
habvi <a2231250601h@yahoo.co.jp>
2022-09-03 17:12:05 +0900

    refactor

--- AGC/034/B_ABC.py
+++ AGC/034/B_ABC.py
@@ -1,23 +1,13 @@
-s = input()
-s = s.replace('BC', 'X')
+S = input()
 
-S = []
-for t in s.split('C'):
-    if not t:
-        continue
-    for u in t.split('B'):
-        if not u:
-            continue
-        u = u.lstrip('X').rstrip('A')
-        if u:
-            S.append(u)
-
+S = S.replace("BC", "X")
 ans = 0
-for s in S:
-    cnt = 0
-    for t in s:
-        if t == 'A':
-            cnt += 1
-        else:
-            ans += cnt
+for s in S.split('C'):
+    for t in s.split('B'):
+        a = 0
+        for u in t:
+            if u == 'A':
+                a += 1
+            else:
+                ans += a
 print(ans)
