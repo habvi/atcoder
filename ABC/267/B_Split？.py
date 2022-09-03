54c0b4f3e0ee0f6613d9c96f95e55f3ce5946897
habvi <a2231250601h@yahoo.co.jp>
2022-09-04 00:22:04 +0900

    refactor

--- ABC/267/B_Split？.py
+++ ABC/267/B_Split？.py
@@ -6,15 +6,10 @@ if S[0] == '1':
     exit()
 
 for i in range(7):
-    for j in range(i + 1, 7):
-        if j - i <= 1:
-            continue
-        if any(S[b] == '1' for b in B[i]) and any(S[b] == '1' for b in B[j]):
+    for j in range(i + 2, 7):
+        if any(S[k] == '1' for k in B[i]) and any(S[k] == '1' for k in B[j]):
             for k in range(i + 1, j):
-                for b in B[k]:
-                    if S[b] == '1':
-                        break
-                else:
+                if all(S[l] == '0' for l in B[k]):
                     print("Yes")
                     exit()
 print("No")
