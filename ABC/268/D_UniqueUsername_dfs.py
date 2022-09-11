4ad0bf86f7a30b7d4fa38e6c738a817dbd9e2cfe
habvi <a2231250601h@yahoo.co.jp>
2022-09-11 14:33:12 +0900

    fix: dfs

--- ABC/268/D_UniqueUsername_dfs.py
+++ ABC/268/D_UniqueUsername_dfs.py
@@ -1,12 +1,11 @@
 from itertools import permutations
 
-def dfs(A):
+def dfs(A, res):
     if len(A) == N - 1:
-        if sum(A) <= ub_total:
-            ub_all.append(A)
+        ub_all.append(A)
         return
-    for i in range(1, ub_total):
-        dfs(A + [i])
+    for i in range(1, res + 1):
+        dfs(A + [i], res - i)
 
 
 N, M = map(int, input().split())
@@ -14,13 +13,13 @@ S = [input() for _ in range(N)]
 T = set(input() for _ in range(M))
 
 if N == 1:
-    print(S[0] if S[0] not in T and len(S[0]) >= 3 else -1)
+    s = S[0]
+    print(s if s not in T and len(s) >= 3 else -1)
     exit()
 
 ub_total = 16 - sum(map(len, S))
 ub_all = []
-for i in range(1, ub_total + 1):
-    dfs([i])
+dfs([], ub_total)
 
 for per in permutations(S):
     for ub in ub_all:
