b38ecd33833b31828eb71d8d11367ca6cc787f86
habvi <a2231250601h@yahoo.co.jp>
2022-09-13 01:51:06 +0900

    style

--- ABC/135/D_DigitsParade.py
+++ ABC/135/D_DigitsParade.py
@@ -1,6 +1,6 @@
-def update(now):
+def update(num):
     for pre, num in enumerate(dp):
-        nxt = (now * k + pre) % m
+        nxt = (pre + num * power) % m
         ndp[nxt] += num
         ndp[nxt] %= MOD
 
@@ -11,18 +11,16 @@ MOD = 10**9 + 7
 m = 13
 dp = [0] * m
 dp[0] = 1
-k = 1
+power = 1
 for s in S[::-1]:
     ndp = [0] * m
     if s == '?':
-        for now in range(10):
-            update(now)
+        for num in range(10):
+            update(num)
     else:
-        now = int(s)
-        update(now)
-
-    k *= 10
-    k %= m
+        num = int(s)
+        update(num)
+    power *= 10
+    power %= m
     dp = ndp
-
 print(dp[5])
