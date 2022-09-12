8c6b2c6d17114917a3b60fe3fd51306087978626
habvi <a2231250601h@yahoo.co.jp>
2022-09-13 01:40:49 +0900

    refactor

--- ABC/259/C_XXtoXXX.py
+++ ABC/259/C_XXtoXXX.py
@@ -1,24 +1,19 @@
 from itertools import groupby
 
-def gen(S):
-    a1 = []
-    a2 = []
-    for k, v in groupby(S):
-        lv = len(list(v))
-        a1.append((k, lv))
-        if lv >= 2:
-            lv = 2
-        a2.append(lv)
-    return a1, a2
+def solve():
+    gs = [(k, len(list(v))) for k, v in groupby(S)]
+    gt = [(k, len(list(v))) for k, v in groupby(T)]
+    if len(gs) != len(gt):
+        return False
+
+    for (s, sv), (t, tv) in zip(gs, gt):
+        if s != t or sv > tv:
+            return False
+        if sv == 1 and tv != 1:
+            return False
+    return True
 
 
 S = input()
 T = input()
-
-s1, s2 = gen(S)
-t1, t2 = gen(T)
-for (sk, sl), (tk, tl) in zip(s1, t1):
-    if sk !=  tk or sl > tl:
-        print('No')
-        exit()
-print('Yes' if s2 == t2 else 'No')
+print("Yes" if solve() else "No")
