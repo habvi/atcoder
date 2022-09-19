9472d66555c194edb41c5216bd075e0468002e5b
habvi <a2231250601h@yahoo.co.jp>
2022-09-19 13:41:15 +0900

    refactor: bisect

--- ABC/269/E_LastRook.py
+++ ABC/269/E_LastRook.py
@@ -1,56 +1,30 @@
-def calc(a, b):
-    diff = b - a
-    if diff % 2:
-        diff //= 2
-        ll, lr = a, a + diff
-        rl, rr = lr + 1, b
-        return ll, lr, rl, rr, diff + 1, diff + 1
-    else:
-        diff //= 2
-        ll, lr = a, a + diff
-        rl, rr = lr + 1, b
-        return ll, lr, rl, rr, diff + 1, diff
+def is_ok(l, r):
+    T = int(input())
+    return T != (r - l) 
+
+def bisect1(l, r):
+    while abs(r - l) > 1:
+        mid = (r + l) // 2
+        print('?', l, mid - 1, 1, N)
+        if is_ok(l, mid):
+            r = mid
+        else:
+            l = mid
+    return l
+
+def bisect2(l, r):
+    while abs(r - l) > 1:
+        mid = (r + l) // 2
+        print('?', 1, N, l, mid - 1)
+        if is_ok(l, mid):
+            r = mid
+        else:
+            l = mid
+    return l
 
 
 N = int(input())
 
-col = None
-l, r = 1, N
-ll, lr, rl, rr, l_num, r_num = calc(l, r)
-left, right = ll, lr
-while col is None:
-    print("?", 1, N, left, right)
-    T = int(input())
-    if T == 0:
-        col = left
-        break
-    if l_num == r_num == 1:
-        col = left + 1
-        break
-    if T == l_num:
-        ll, lr, rl, rr, l_num, r_num = calc(rl, rr)
-        left, right = ll, lr
-    else:
-        ll, lr, rl, rr, l_num, r_num = calc(left, right)
-        left, right = ll, lr
-
-row = None
-l, r = 1, N
-ll, lr, rl, rr, l_num, r_num = calc(l, r)
-left, right = ll, lr
-while row is None:
-    print("?", left, right, 1, N)
-    T = int(input())
-    if T == 0:
-        row = left
-        break
-    if l_num == r_num == 1:
-        row = left + 1
-        break
-    if T == l_num:
-        ll, lr, rl, rr, l_num, r_num = calc(rl, rr)
-        left, right = ll, lr
-    else:
-        ll, lr, rl, rr, l_num, r_num = calc(left, right)
-        left, right = ll, lr
+row = bisect1(1, N + 1)
+col = bisect2(1, N + 1)
 print("!", row, col)
