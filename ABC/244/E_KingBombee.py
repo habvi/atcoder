f03be8eef062e216ff1ba342b111f2b5bbfb5beb
habvi <a2231250601h@yahoo.co.jp>
2022-09-22 23:20:43 +0900

    refactor

--- ABC/244/E_KingBombee.py
+++ ABC/244/E_KingBombee.py
@@ -1,30 +1,28 @@
 from collections import defaultdict
 
-n, m, K, S, T, X = map(int, input().split())
+N, M, K, S, T, X = map(int, input().split())
 S, T, X = S - 1, T - 1, X - 1
 MOD = 998244353
 
 g = defaultdict(list)
-for _ in range(m):
+for _ in range(M):
     a, b = map(lambda x: int(x) - 1, input().split())
     g[a].append(b)
     g[b].append(a)
 
-dp = [[0] * 2 for _ in range(n)]
-dp[S][0] = 1
-
+dp = [[0] * N for _ in range(2)]
+dp[0][S] = 1
 for _ in range(K):
-    ndp = [[0] * 2 for _ in range(n)]
-    for v in range(n):
+    ndp = [[0] * N for _ in range(2)]
+    for v in range(N):
         for nv in g[v]:
-            if nv == X:
-                ndp[nv][0] += dp[v][1]
-                ndp[nv][1] += dp[v][0]
+            if v == X:
+                ndp[0][nv] += dp[1][v]
+                ndp[1][nv] += dp[0][v]
             else:
-                ndp[nv][0] += dp[v][0]
-                ndp[nv][1] += dp[v][1]
-            ndp[nv][0] %= MOD
-            ndp[nv][1] %= MOD
-    dp = ndp
-
-print(dp[T][0])
+                ndp[0][nv] += dp[0][v]
+                ndp[1][nv] += dp[1][v]
+            ndp[0][nv] %= MOD
+            ndp[1][nv] %= MOD
+    dp = [d[:] for d in ndp]
+print(dp[0][T])
